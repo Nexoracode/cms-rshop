e81c9a05314bb5e7970fff11e12acc13f213f18a
khadem-mh <khadem-mh@RHAC.local>
2025-06-09 12:51:57 +0330

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
