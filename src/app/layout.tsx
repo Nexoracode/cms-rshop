66d4977ada4d56397d8dd041022b36ca107e09a9
mh.kadem <khadem5939@gmail.com>
2025-06-11 12:55:19 +0330

    Test - 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "@styles/globals.css";
+import "../styles/globals.css";
 import { Toaster } from 'react-hot-toast';
 import GetThemeSite from "@comp_global/GetThemeSite";
 import AuthProvider from "@/components/auth/modules/AuthProvider";
