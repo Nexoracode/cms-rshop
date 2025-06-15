bb94f9fb2c7a8a21f8e97a9a4d9b94d7b13ef4c9
mh.kadem <khadem5939@gmail.com>
2025-06-15 14:05:04 +0330

    Design Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import Link from "next/link"
+
 const HomePage = () => {
-    return ( <></> )
+    return (
+        <div className="fixed inset-0">
+            <div className="absolute inset-0 animate-pulse bg-splash-screen" style={{ animationDuration: '10000ms' }}></div>
+
+            <div className="absolute inset-0 flex items-center justify-center">
+                <div className="text-center relative z-10">
+                    <img
+                        src="/icons/logo.png"
+                        alt="Logo"
+                        className="w-52 mx-auto mb-6"
+                    />
+                    <h1 className="text-3xl text-white font-bold select-none font-title mb-24">به پنل مدیرتی فروشگاه آرشاپ خوش آمدید</h1>
+                    <Link href={'/signin'} className="underline mt-24">ورود به حساب کاربری</Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default HomePage
--- src/components/global/SplashScreen/ContentScreen.tsx
+++ src/components/global/SplashScreen/ContentScreen.tsx
@@ -2,9 +2,9 @@ const ContentScreen = () => {
     return (
         <div className="text-center">
             <img
-                src="/icons/icon-192x192.png"
+                src="/icons/logo.png"
                 alt="Logo"
-                className="w-26 h-26 mx-auto mb-6 rounded-lg animate-pulse"
+                className="w-52 mx-auto mb-6 animate-pulse"
             />
             <h1 className="text-3xl text-white font-bold select-none animate-pulse font-title">پنل مدیریتی فروشگاه آرشاپ</h1>
         </div>
