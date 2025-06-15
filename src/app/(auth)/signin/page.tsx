50d589aa402338547a624c5eb0f332cb70352ba2
mh.kadem <khadem5939@gmail.com>
2025-06-15 11:02:24 +0330

    Update login page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -13,29 +13,32 @@ import PhoneInput from "@/components/auth/modules/PhoneInput"
 const SignIn = () => {
 
   const router = useRouter()
+  const [isShowOtp, setIsShowOtp] = useState(false)
   const [infos, setInfos] = useState({
     phone: "",
     otp: "",
   })
 
-  const handleSubmit = async () => {
+  const handleSendVerificationCode = async () => {
+    if (isOnline()) return
+    const toastPhone = toast.loading("درحال ارسال کد به تلفن همراه...")
+    const resPhone = await sendVerificationCode(infos.phone) // call to action
+    toast.dismiss(toastPhone)
+  }
+
+  const handleVerifyCode = async () => {
     if (isOnline()) return
     const { otp, phone } = infos
-    if (!otp) {
-      const toastPhone = toast.loading("درحال ارسال کد به تلفن همراه...")
-      const resPhone = await sendVerificationCode(phone) // call to action
-      toast.dismiss(toastPhone)
-    } else {
-      const toastVerify = toast.loading("در حال بررسی کد...")
-      const resVerify = await verifyCode(phone, otp) // call to action
-      toast.dismiss(toastVerify)
-    }
+    const toastVerify = toast.loading("در حال بررسی کد...")
+    const resVerify = await verifyCode(phone, otp) // call to action
+    toast.dismiss(toastVerify)
   }
 
   return (
-    infos.otp ?
+    isShowOtp ?
       <div>
         <Header title="خوش آمدید" subTitle="کد ارسال شده را وارد نمایید" />
+        <AuthButton onClickHandler={handleVerifyCode} title="بررسی کد" disable={infos.otp.length ? false : true} />
       </div>
       :
       <div>
@@ -45,7 +48,7 @@ const SignIn = () => {
             onValidPhoneChange={phone => setInfos(prev => ({ ...prev, phone: phone }))}
           />
         </form>
-        <AuthButton onClickHandler={handleSubmit} title="ارسال کد" disable={infos.phone.length ? false : true} />
+        <AuthButton onClickHandler={handleSendVerificationCode} title="ارسال کد" disable={infos.phone.length ? false : true} />
       </div>
   );
 };
