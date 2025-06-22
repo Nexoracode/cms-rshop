1f31d02e8576edcd0ab3426b861c2dff38109f0d
mh.kadem <khadem5939@gmail.com>
2025-06-22 14:55:58 +0330

    Update SideBar

--- src/components/Admin/Sidebar/Item.tsx
+++ src/components/Admin/Sidebar/Item.tsx
@@ -1,21 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
 
 type Props = {
-    title: string,
-    routeName: string,
-    icon: React.ReactNode
-}
+    title: string;
+    routeName: string;
+    icon: React.ReactNode;
+    parentStyle: string,
+    iconStyle: string
+};
 
-const Item = ({ title, routeName, icon }: Props) => {
-
+const Item = ({ title, routeName, icon, iconStyle, parentStyle }: Props) => {
     return (
-        <Link href={`/admin/${routeName}`} className="flex items-center justify-end text-md px-2 py-2 rounded-md hover:bg-green-700/5 transition hover:text-green-700">
+        <Link
+            href={`/admin/${routeName}`}
+            className={`flex items-center justify-end text-md px-2 py-2 rounded-md transition ${parentStyle}`}
+        >
             <span className="px-2">{title}</span>
-            <div className="bg-green-700/10 text-green-700 rounded-md p-1">
+            <div className={`rounded-md p-1 ${iconStyle}`}>
                 {icon}
             </div>
         </Link>
-    )
-}
+    );
+};
 
-export default Item
+export default Item;
--- src/components/Admin/Sidebar/Sidebar.tsx
+++ src/components/Admin/Sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import React from "react";
 import { HiOutlineHome } from "react-icons/hi";
+import { IoReceiptOutline, IoSettingsOutline } from "react-icons/io5";
+import { AiOutlineShop } from "react-icons/ai";
+import Item from "./Item";
 
 export default function Sidebar() {
     return (
@@ -12,21 +15,34 @@ export default function Sidebar() {
             </div>
 
             <nav className="flex flex-col gap-4 text-end mt-4">
-                <Link href="/admin/home" className="flex items-center justify-end text-md px-2 py-2 rounded-md hover:bg-green-700/5 transition hover:text-green-700">
-                    <span className="px-2">خانه</span>
-                    <div className="bg-green-700/10 text-green-700 rounded-md p-1">
-                        <HiOutlineHome className="text-2xl" />
-                    </div>
-                </Link>
-                <Link href="/admin/orders" className="text-sm font-medium">
-                    سفارشات
-                </Link>
-                <Link href="/admin/products" className="text-sm font-medium">
-                    محصولات
-                </Link>
-                <Link href="/admin/settings" className="text-sm font-medium">
-                    تنظیمات
-                </Link>
+                <Item
+                    title="خانه"
+                    icon={<HiOutlineHome className="text-2xl" />}
+                    routeName="home"
+                    parentStyle="text-green-700 hover:text-green-700 hover:bg-green-700/5"
+                    iconStyle="bg-green-700/10"
+                />
+                <Item
+                    title="سفارشات"
+                    icon={<IoReceiptOutline className="text-2xl" />}
+                    routeName="orders"
+                    parentStyle="text-blue-700 hover:text-blue-700 hover:bg-blue-700/5"
+                    iconStyle="bg-blue-700/10"
+                />
+                <Item
+                    title="محصولات"
+                    icon={<AiOutlineShop className="text-2xl" />}
+                    routeName="products"
+                    parentStyle="text-orange-700 hover:text-orange-700 hover:bg-orange-700/5"
+                    iconStyle="bg-orange-700/10"
+                />
+                <Item
+                    title="تنظیمات"
+                    icon={<IoSettingsOutline className="text-2xl" />}
+                    routeName="settings"
+                    parentStyle="text-sky-700 hover:text-sky-700 hover:bg-sky-700/5"
+                    iconStyle="bg-sky-700/10"
+                />
             </nav>
         </aside>
     );
