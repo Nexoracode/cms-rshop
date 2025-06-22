4d68ddf425a17e45c6b4a6f0e2b53cacae793e2f
mh.kadem <khadem5939@gmail.com>
2025-06-22 12:11:58 +0330

    Update Signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,35 +1,45 @@
-"use client"
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Tabs, Tab, Card, CardBody, InputOtp, Input } from "@heroui/react";
 
+type auth = "phone" | "otp"
+
 export default function App() {
-    const [selected, setSelected] = React.useState("phone");
+    const [selected, setSelected] = useState<auth>("phone");
 
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex flex-col items-center justify-center mb-6">
                 <img src="/images/logo.png" alt="logo" className="w-32 mb-3" />
-                <p>شماره تماس خود را وارد نمایید</p>
+                <p>
+                    {
+                        selected === "phone" ? "شماره تماس خود را وارد نمایید" : "کد ارسال شده به تلفن خود را وارد نمایید"
+                    }
+                </p>
             </div>
-            <Card className="max-w-[550px] w-full">
+
+            <Card className="max-w-[450px] w-full">
                 <CardBody className="overflow-hidden">
                     <Tabs
-                        fullWidth
+                        isVertical={true}
                         aria-label="Tabs form"
                         selectedKey={selected}
                         size="md"
-                        onSelectionChange={(key) => setSelected(String(key))}
-                        className="mb-6"
+                        onSelectionChange={(key) => setSelected(key as auth)}
                     >
-                        <Tab key="otp" title="رمز یک‌ بار مصرف">
-                            <div className="flex items-center justify-center">
-                                <InputOtp length={6} size="lg" />
-                            </div>
+                        <Tab key="phone" title="شماره تماس" className="flex items-center justify-center w-full">
+                            <Input
+                                label="شماره تماس"
+                                type="tel"
+                                inputMode="tel"
+                                variant="flat"
+                                size="sm"
+                                maxLength={11}
+                            />
                         </Tab>
-                        <Tab key="phone" title="شماره تماس">
-                            <div className="flex items-center justify-center">
-                                <Input label="شماره تماس" type="tel" inputMode="tel" variant="flat" size="sm" maxLength={11} />
-                            </div>
+                        <Tab key="otp" title="رمز یک‌بار مصرف" className="flex items-center justify-center w-full">
+                            <InputOtp length={6} size="md" />
                         </Tab>
                     </Tabs>
                 </CardBody>
@@ -37,4 +47,3 @@ export default function App() {
         </div>
     );
 }
-
