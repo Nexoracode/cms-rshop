b6a696e5ad2b65609102a74b3886caf514a8eb43
mh.kadem <khadem5939@gmail.com>
2025-06-22 13:03:38 +0330

    re-update signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -7,72 +7,73 @@ import BreakpointWatcher from "@/components/Helper/BreakpointWatcher";
 type Auth = "phone" | "otp";
 
 export default function App() {
-  const [selected, setSelected] = useState<Auth>("phone");
-  const [isPhone, setIsPhone] = useState(false);
-  const [phoneValue, setPhoneValue] = useState("");
+    const [selected, setSelected] = useState<Auth>("phone");
+    const [isPhone, setIsPhone] = useState(false);
+    const [phoneValue, setPhoneValue] = useState("");
 
-  const handleTabChange = (key: string) => {
-    if (key === "otp" && phoneValue.length < 11) return;
-    setSelected(key as Auth);
-  };
+    const handleTabChange = (key: string) => {
+        if (key === "otp" && phoneValue.length < 11) return;
+        setSelected(key as Auth);
+    };
 
-  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let val = e.target.value;
-    val = val.replace(/\D/g, "");
-    setPhoneValue(val);
+    const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let val = e.target.value;
+        val = val.replace(/\D/g, "");
+        setPhoneValue(val);
 
-    if (val.length === 11) setSelected("otp");
-  };
+        if (val.length === 11) setSelected("otp");
+    };
 
-  return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-128px)]">
-      <div className="flex flex-col items-center justify-center mb-6">
-        <img src="/images/logo.png" alt="logo" className="w-32 mb-3" />
-        <p>
-          {selected === "phone"
-            ? "شماره تماس خود را وارد نمایید"
-            : "کد ارسال شده به تلفن خود را وارد نمایید"}
-        </p>
-      </div>
+    return (
+        <div className="flex flex-col items-center justify-center h-[calc(100vh-128px)]">
+            <div className="flex flex-col items-center justify-center mb-6">
+                <img src="/images/logo.png" alt="logo" className="w-32 mb-3" />
+                <p>
+                    {selected === "phone"
+                        ? "شماره تماس خود را وارد نمایید"
+                        : "کد ارسال شده به تلفن خود را وارد نمایید"}
+                </p>
+            </div>
 
-      <Card className="max-w-[450px] w-full">
-        <CardBody className="overflow-hidden">
-          <Tabs
-            isVertical={isPhone}
-            aria-label="Tabs form"
-            selectedKey={selected}
-            size="md"
-            onSelectionChange={(key) => handleTabChange(key as Auth)}
-            className={`flex ${!isPhone ? "justify-center" : ""}`}
-          >
-            <Tab
-              key="phone"
-              title="شماره تماس"
-              className="flex items-center justify-center w-full"
-            >
-              <Input
-                label="شماره تماس"
-                type="tel"
-                inputMode="tel"
-                variant="flat"
-                size="sm"
-                maxLength={11}
-                value={phoneValue}
-                onChange={handlePhoneChange}
-              />
-            </Tab>
-            <Tab
-              key="otp"
-              title="رمز یک‌بار مصرف"
-              className="flex items-center justify-center w-full"
-            >
-              <InputOtp length={6} size="md" />
-            </Tab>
-          </Tabs>
-        </CardBody>
+            <Card className="max-w-[450px] w-full">
+                <CardBody className="overflow-hidden">
+                    <Tabs
+                        isVertical={isPhone}
+                        aria-label="Tabs form"
+                        selectedKey={selected}
+                        size="md"
+                        onSelectionChange={(key) => handleTabChange(key as Auth)}
+                        className={`flex ${!isPhone ? "justify-center" : ""}`}
+                    >
+                        <Tab
+                            key="phone"
+                            title="شماره تماس"
+                            className="flex items-center justify-center w-full"
+                        >
+                            <Input
+                                autoFocus
+                                label="شماره تماس"
+                                type="tel"
+                                inputMode="tel"
+                                variant="flat"
+                                size="sm"
+                                maxLength={11}
+                                value={phoneValue}
+                                onChange={handlePhoneChange}
+                            />
+                        </Tab>
+                        <Tab
+                            key="otp"
+                            title="رمز یک‌بار مصرف"
+                            className="flex items-center justify-center w-full"
+                        >
+                            <InputOtp length={6} size="md" autoFocus />
+                        </Tab>
+                    </Tabs>
+                </CardBody>
 
-        <BreakpointWatcher onBreakpointChange={(status) => setIsPhone(status)} />
-      </Card>
-    </div>
-  );
+                <BreakpointWatcher onBreakpointChange={(status) => setIsPhone(status)} />
+            </Card>
+        </div>
+    );
 }
