d684c9a5b04f3eee8b5982199f6f7a6463ef7d41
mh.kadem <khadem5939@gmail.com>
2025-06-22 11:53:08 +0330

    Test

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -6,7 +6,11 @@ export default function App() {
     const [selected, setSelected] = React.useState("phone");
 
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center mb-6">
+                <img src="/images/logo.png" alt="logo" className="w-32 mb-3" />
+                <p>شماره تماس خود را وارد نمایید</p>
+            </div>
             <Card className="max-w-[550px] w-full">
                 <CardBody className="overflow-hidden">
                     <Tabs
@@ -23,11 +27,7 @@ export default function App() {
                             </div>
                         </Tab>
                         <Tab key="phone" title="شماره تماس">
-                            <div className="flex flex-col items-center justify-center">
-                                <div className="flex flex-col items-center justify-center mb-6">
-                                    <img src="/images/logo.png" alt="logo" className="w-32 mb-3" />
-                                    <p>شماره تماس خود را وارد نمایید</p>
-                                </div>
+                            <div className="flex items-center justify-center">
                                 <Input label="شماره تماس" type="tel" inputMode="tel" variant="flat" size="sm" maxLength={11} />
                             </div>
                         </Tab>
