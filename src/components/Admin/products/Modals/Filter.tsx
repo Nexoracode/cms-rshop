24a64fed00a30d3bcb4fc1f1b1cc5745d5386f9d
mh.kadem <khadem5939@gmail.com>
2025-06-23 14:47:51 +0330

    Update Filter

--- src/components/Admin/products/Modals/Filter.tsx
+++ src/components/Admin/products/Modals/Filter.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { NumberInput, Select, SelectItem } from "@heroui/react";
+import { Checkbox, NumberInput, Select, SelectItem } from "@heroui/react";
 import { Modal, ModalContent, ModalHeader, ModalBody } from "@heroui/react";
 
 type Props = {
@@ -58,7 +58,7 @@ const Filter: React.FC<Props> = ({ isOpen, onOpenChange }) => {
                                         label="از"
                                         placeholder="0.00"
                                         labelPlacement={"outside-left"}
-                                        endContent={
+                                        startContentContent={
                                             <div className="pointer-events-none flex items-center">
                                                 <span className="text-default-400 text-small">تومان</span>
                                             </div>
@@ -75,6 +75,94 @@ const Filter: React.FC<Props> = ({ isOpen, onOpenChange }) => {
                                         }
                                     />
                                 </div>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <p className="-mb-2 text-black/80">تخفیف</p>
+                                <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                                    <NumberInput
+                                        label="از"
+                                        placeholder="0.00"
+                                        labelPlacement={"outside-left"}
+                                        endContent={
+                                            <div className="pointer-events-none flex items-center">
+                                                <span className="text-default-400 text-small">%</span>
+                                            </div>
+                                        }
+                                    />
+                                    <NumberInput
+                                        label="تا"
+                                        labelPlacement={"outside-left"}
+                                        placeholder="0.00"
+                                        endContent={
+                                            <div className="pointer-events-none flex items-center">
+                                                <span className="text-default-400 text-small">%</span>
+                                            </div>
+                                        }
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                                    <NumberInput
+                                        hideStepper
+                                        label="حداقل از وزن"
+                                        placeholder="0.5"
+                                        labelPlacement={"outside"}
+                                        endContent={
+                                            <div className="flex items-center">
+                                                <label className="sr-only" htmlFor="currency">
+                                                    Currency
+                                                </label>
+                                                <select
+                                                    aria-label="Select currency"
+                                                    className="outline-none border-0 bg-transparent text-default-400 text-small"
+                                                    defaultValue="USD"
+                                                    id="currency"
+                                                    name="currency"
+                                                >
+                                                    <option aria-label="Argentine Peso" value="ARS">
+                                                        گرم
+                                                    </option>
+                                                    <option aria-label="US Dollar" value="USD">
+                                                        کیلوگرم
+                                                    </option>
+                                                </select>
+                                            </div>
+                                        }
+                                    />
+                                    <NumberInput
+                                        hideStepper
+                                        endContent={
+                                            <div className="flex items-center">
+                                                <label className="sr-only" htmlFor="currency">
+                                                    Currency
+                                                </label>
+                                                <select
+                                                    aria-label="Select currency"
+                                                    className="outline-none border-0 bg-transparent text-default-400 text-small"
+                                                    defaultValue="USD"
+                                                    id="currency"
+                                                    name="currency"
+                                                >
+                                                    <option aria-label="Argentine Peso" value="ARS">
+                                                        گرم
+                                                    </option>
+                                                    <option aria-label="US Dollar" value="USD">
+                                                        کیلوگرم
+                                                    </option>
+                                                </select>
+                                            </div>
+                                        }
+
+                                        labelPlacement={"outside"}
+                                        label="حداکثر تا وزن"
+                                        placeholder="10"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-3 pt-4">
+                                <Checkbox defaultSelected>انتخاب محصولات بدون وزن</Checkbox>
+                                <Checkbox defaultSelected>انتخاب محصولات متصل به دیوار</Checkbox>
                             </div>
                         </ModalBody>
                     </>
