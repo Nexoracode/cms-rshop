05cc742ea883ca5593a4a9932edf8ae554272bbd
mh.kadem <khadem5939@gmail.com>
2025-06-25 11:41:33 +0330

    Update LastAdditionalInfos

--- src/components/Admin/products/NewProduct/LastAdditionalInfos.tsx
+++ src/components/Admin/products/NewProduct/LastAdditionalInfos.tsx
@@ -1,30 +1,53 @@
-"use client"
+"use client";
 
-import { Button, Card, CardBody, useDisclosure } from "@heroui/react"
+import { useState } from "react";
+import { Button, Card, CardBody, useDisclosure } from "@heroui/react";
 import AddNewPropertyModal from "./Modal/AddNewPropertyModal";
 
-const LastAdditionalInfos = () => {
+type Property = {
+    title: string;
+    description: string;
+};
 
+const LastAdditionalInfos = () => {
+    const [properties, setProperties] = useState<Property[]>([]);
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
     return (
         <>
             <Card className="w-full shadow-md">
-                <CardBody dir="rtl" className="flex flex-col gap-6 text-start">
+                <CardBody dir="rtl" className="flex flex-col gap-4 text-start">
                     <div className="w-full px-2 flex items-center justify-between">
                         <span>مشخصات</span>
-                        <Button color="secondary" variant="light" onPress={onOpen}>+ افزودن مشخصات جدید</Button>
+                        <Button color="secondary" variant="light" onPress={onOpen}>
+                            + افزودن مشخصات جدید
+                        </Button>
                     </div>
+
+                    {properties.length > 0 && (
+                        properties.map((prop, index) => (
+                            <Card key={index} className="shadow-md border">
+                                <CardBody className="w-full flex flex-row items-center text-start">
+                                    <div className="w-2/12">
+                                        <p>{prop.title}</p>
+                                    </div>
+                                    <div className="w-10/12">
+                                        <p className="text-gray-600">{prop.description}</p>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        ))
+                    )}
                 </CardBody>
             </Card>
 
             <AddNewPropertyModal
-                onSubmit={(title, description) => console.log(title, description)}
+                onSubmit={(title, description) => setProperties(prev => [...prev, { title, description }])}
                 isOpen={isOpen}
                 onOpenChange={onOpenChange}
             />
         </>
-    )
-}
+    );
+};
 
-export default LastAdditionalInfos
+export default LastAdditionalInfos;
