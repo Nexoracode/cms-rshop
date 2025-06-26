affcf6f212b24a7be0a969743ef4b206040318d5
mh.kadem <khadem5939@gmail.com>
2025-06-26 10:54:03 +0330

    Update AddNewBrandModal

--- src/components/Admin/products/NewProduct/Modal/AddNewBrandModal.tsx
+++ src/components/Admin/products/NewProduct/Modal/AddNewBrandModal.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Button, Chip, Input, ModalFooter } from "@heroui/react";
 import { Modal, ModalContent, ModalHeader, ModalBody } from "@heroui/react";
 import { TbCategoryPlus } from "react-icons/tb";
+import ImageBoxUploader from "../ImageBoxUploader";
 
 type Props = {
     isOpen: boolean;
@@ -32,7 +33,7 @@ const AddNewBrandModal: React.FC<Props> = ({
         setImageFile(defaultValues?.imageFile || null);
     }, [defaultValues]);
 
-    const isDisabled = !brandFa.trim() || !brandEn.trim();
+    const isDisabled = !brandFa.trim() || !brandEn.trim() || !imageFile;
 
     const handleSubmit = () => {
         if (!isDisabled) {
@@ -41,13 +42,6 @@ const AddNewBrandModal: React.FC<Props> = ({
             setBrandEn("");
             setImageFile(null);
             onOpenChange();
-        }
-    };
-
-    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (file && file.size <= 5 * 1024 * 1024) {
-            setImageFile(file);
         }
     };
 
@@ -80,55 +74,12 @@ const AddNewBrandModal: React.FC<Props> = ({
                                 value={brandEn}
                                 onChange={(e) => setBrandEn(e.target.value)}
                             />
-                            <div>
-                                <div className="w-full px-2 flex items-center justify-between my-3">
-                                    <span>تصویر لوگو</span>
-                                    <input
-                                        type="file"
-                                        accept="image/*"
-                                        id="logo-upload"
-                                        onChange={handleImageChange}
-                                        className="bg-black z-50 h-10 absolute left-0 opacity-0"
-                                    />
-                                    <label htmlFor="logo-upload">
-                                        <p className="ml-2 text-[var(--primary)] transition">
-                                            {imageFile ? "تغییر لوگو" : "+ افزودن لوگو"}
-                                        </p>
-                                    </label>
-                                </div>
-
-                                <div className="flex items-start gap-3">
-                                    <div className="w-[80px] h-[80px] border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center">
-                                        <div className={`${!imageFile ? "p-3 bg-gray-100 rounded-lg" : ""}`}>
-                                            {
-                                                imageFile ?
-                                                    <img
-                                                        src={URL.createObjectURL(imageFile)}
-                                                        alt="preview"
-                                                        className="rounded-2xl w-[80px] h-[80px] p-2 object-cover"
-                                                    />
-                                                    :
-                                                    <TbCategoryPlus className="text-4xl text-gray-500" />
-                                            }
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col text-[12px] gap-1 text-gray-500">
-                                        <p>نمایش تصویر پیش فرض به این شکل است.</p>
-                                        <div>فرمت تصویر:
-                                            <Chip color="secondary" variant="flat" size="sm" radius="sm">
-                                                <small>JPEG</small>
-                                            </Chip>  ,
-                                            <Chip color="success" variant="flat" size="sm" radius="sm">
-                                                <small>JPG</small>
-                                            </Chip>  ,
-                                            <Chip color="warning" variant="flat" size="sm" radius="sm">
-                                                <small>PNG</small>
-                                            </Chip>
-                                        </div>
-                                        <p>سایز تصویر: 160x160</p>
-                                    </div>
-                                </div>
-                            </div>
+                            <ImageBoxUploader
+                                textBtn={imageFile ? "تغییر لوگو" : "+ افزودن لوگو"}
+                                title="تصویر لوگو"
+                                changeStatusFile={imageFile}
+                                onFile={(file) => setImageFile(file)}
+                            />
                         </ModalBody>
                         <ModalFooter>
                             <Button
