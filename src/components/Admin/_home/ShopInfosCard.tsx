f8a4291c4348e4078d6e0c5d6ced84b9e4670068
mh.kadem <khadem5939@gmail.com>
2025-07-03 13:22:12 +0330

    Update ShopInfos

--- src/components/Admin/_home/ShopInfosCard.tsx
+++ src/components/Admin/_home/ShopInfosCard.tsx
@@ -11,15 +11,9 @@ import {
 } from "@heroui/react"
 import { MdMoreVert } from "react-icons/md"
 import ArShopCard from "../ArShopCard"
-import { RxLapTimer } from "react-icons/rx";
-import { TbEyeShare } from "react-icons/tb";
-import { BsCopy } from "react-icons/bs";
-
-export const ListboxWrapper = ({ children }: { children: React.ReactNode }) => (
-    <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
-        {children}
-    </div>
-);
+import { RxLapTimer } from "react-icons/rx"
+import { TbEyeShare } from "react-icons/tb"
+import { BsCopy } from "react-icons/bs"
 
 const ShopInfosCard = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -44,28 +38,26 @@ const ShopInfosCard = () => {
                         <MdMoreVert className="text-lg" /> بیشتر
                     </Button>
                 </PopoverTrigger>
-                <PopoverContent className="w-[240px]">
+                <PopoverContent className="w-[240px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
                     <div className="flex flex-col items-center mt-3 gap-1">
                         <p>فروشگاه آرشاپ</p>
                         <small className="text-gray-600">arshop.ir</small>
                     </div>
-                    <div className="p-4">
-                        <ListboxWrapper>
-                            <Listbox aria-label="Listbox menu with icons" variant="faded">
-                                <ListboxItem key="visit" startContent={<TbEyeShare className="text-xl" />}>
-                                    مشاهده فروشگاه
-                                </ListboxItem>
-                                <ListboxItem showDivider key="copy" startContent={<BsCopy className="text-xl" />}>
-                                    کپی لینک فروشگاه
-                                </ListboxItem>
-                                <ListboxItem
-                                    key="disable-order"
-                                    startContent={<RxLapTimer className="text-xl" />}
-                                >
-                                    غیرفعال سازی سفارش گیری
-                                </ListboxItem>
-                            </Listbox>
-                        </ListboxWrapper>
+                    <div className="mt-4">
+                        <Listbox aria-label="Listbox menu with icons" variant="faded">
+                            <ListboxItem key="visit" startContent={<TbEyeShare className="text-[var(--primary)] text-xl" />}>
+                                مشاهده فروشگاه
+                            </ListboxItem>
+                            <ListboxItem showDivider key="copy" startContent={<BsCopy className="text-[var(--primary)] text-xl" />}>
+                                کپی لینک فروشگاه
+                            </ListboxItem>
+                            <ListboxItem
+                                key="disable-order"
+                                startContent={<RxLapTimer className="text-[var(--primary)] text-xl" />}
+                            >
+                                غیرفعال سازی سفارش گیری
+                            </ListboxItem>
+                        </Listbox>
                     </div>
                 </PopoverContent>
             </Popover>
