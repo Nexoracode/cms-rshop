8300d3bafb04333d7af72469e333c73ae871f2fe
mh.kadem <khadem5939@gmail.com>
2025-07-07 14:56:54 +0330

    Update

--- src/components/Helper/MediaGallery.tsx
+++ src/components/Helper/MediaGallery.tsx
@@ -3,59 +3,59 @@
 import { Button, Modal, ModalContent, Tab, Tabs } from "@heroui/react"
 import { LuImages, LuUpload } from "react-icons/lu"
 import { useState } from "react"
+import ImageCropper from "./ImageCropper"
 
 const MediaGallery = () => {
-  const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
-  return (
-    <div>
-      <Button
-        color="secondary"
-        variant="flat"
-        size="sm"
-        className="rounded-md"
-        onPress={() => setIsOpen(true)}
-      >
-        <LuImages className="text-lg" /> گالری
-      </Button>
+    return (
+        <div>
+            <Button
+                color="secondary"
+                variant="flat"
+                size="sm"
+                className="rounded-md"
+                onPress={() => setIsOpen(true)}
+            >
+                <LuImages className="text-lg" /> گالری
+            </Button>
 
-      <Modal isOpen={isOpen} onOpenChange={setIsOpen} placement="auto">
-        <ModalContent>
-          {onClose => (
-            <div className="flex w-full flex-col p-2">
-              <Tabs aria-label="Media Options" color="primary" variant="bordered">
-                <Tab
-                  key="gallery"
-                  title={
-                    <div className="flex items-center gap-2">
-                      <LuImages />
-                      <span>گالری</span>
-                    </div>
-                  }
-                >
-                  {/* اینجا محتوای گالری قرار بگیره */}
-                  <div className="p-4">محتوای گالری اینجاست.</div>
-                </Tab>
+            <Modal isOpen={isOpen} size="5xl" onOpenChange={setIsOpen} placement="auto">
+                <ModalContent>
+                    {onClose => (
+                        <div className="flex w-full flex-col p-2">
+                            <Tabs aria-label="Media Options" color="secondary" variant="bordered">
+                                <Tab
+                                    key="upload"
+                                    title={
+                                        <div className="flex items-center gap-2">
+                                            <LuUpload className="text-lg" />
+                                            <span>آپلود فایل</span>
+                                        </div>
+                                    }
+                                >
+                                    <ImageCropper onPreviewsChange={() => {}}/>
+                                </Tab>
 
-                <Tab
-                  key="upload"
-                  title={
-                    <div className="flex items-center gap-2">
-                      <LuUpload />
-                      <span>آپلود فایل</span>
-                    </div>
-                  }
-                >
-                  {/* اینجا فرم آپلود قرار بگیره */}
-                  <div className="p-4">محتوای آپلود فایل اینجاست.</div>
-                </Tab>
-              </Tabs>
-            </div>
-          )}
-        </ModalContent>
-      </Modal>
-    </div>
-  )
+                                <Tab
+                                    key="gallery"
+                                    title={
+                                        <div className="flex items-center gap-2">
+                                            <LuImages className="text-lg" />
+                                            <span>گالری</span>
+                                        </div>
+                                    }
+                                >
+                                    {/* اینجا محتوای گالری قرار بگیره */}
+                                    <div className="p-4">محتوای گالری اینجاست.</div>
+                                </Tab>
+                            </Tabs>
+                        </div>
+                    )}
+                </ModalContent>
+            </Modal>
+        </div>
+    )
 }
 
 export default MediaGallery
