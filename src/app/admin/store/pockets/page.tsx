bdca233dd7cca3a5dcfa92d4b29f3c7d3a9122b5
mh.kadem <khadem5939@gmail.com>
2025-07-09 10:14:36 +0330

    Update pockets page and create new component Packing

--- src/app/admin/store/pockets/page.tsx
+++ src/app/admin/store/pockets/page.tsx
@@ -1,9 +1,21 @@
 "use client"
 
+import HeaderAction from "@/components/Admin/_products/__create/helpers/HeaderAction"
+import BackToPage from "@/components/Helper/BackToPage"
+
 const Pockets = () => {
 
     return (
-        <div></div>
+        <div className="flex flex-col gap-6">
+            <BackToPage title="پیک فروشگاه" link="/admin/store" />
+            <div className="bg-white p-4 rounded-2xl">
+                <HeaderAction
+                    title="تعریف بسته بندی"
+                    textBtn="+ بسته بندی جدید"
+                    onPress={() => { }}
+                />
+            </div>
+        </div>
     )
 
 }
---
+++ src/components/Admin/_store/__pockets/Packaging.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const Packaging = () => {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Packaging
