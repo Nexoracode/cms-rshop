5696748611e85516ac939c4acf9f0d162a1a8b3f
mh.kadem <khadem5939@gmail.com>
2025-07-17 14:31:24 +0330

    Update and Create SubAttributeBox

--- src/components/Admin/_products/__create/AttributesProducts.tsx
+++ src/components/Admin/_products/__create/AttributesProducts.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { Card, CardBody, useDisclosure } from "@heroui/react";
-import { TbBookmarks } from "react-icons/tb";
+import { TbCategory2 } from "react-icons/tb";
 import HeaderAction from "./helpers/HeaderAction";
 import BoxHeader from "./helpers/BoxHeader";
 import { useEffect, useState } from "react";
 import AddNewAttributeModal from "./modals/AddNewAttributeModal/AddNewAttributeModal";
 import { AttributeData } from "./modals/AddNewAttributeModal/Types";
+import SubAttributeBox from "./helpers/SubAttributeBox";
 
 const AttributesProducts = () => {
 
@@ -25,15 +26,23 @@ const AttributesProducts = () => {
                 <BoxHeader
                     title="ویژگی های محصول"
                     color="bg-purple-700/10 text-purple-700"
-                    icon={<TbBookmarks className="text-3xl" />}
+                    icon={<TbCategory2 className="text-3xl" />}
                 />
                 <CardBody dir="rtl" className="flex flex-col gap-4 text-start">
                     <HeaderAction
                         title="ویژگی ها"
-                        textBtn="+ افزودن ویژگی"
+                        textBtn={attributes.length ? "ویرایش ویژگی ها" : "+ افزودن ویژگی"}
                         onPress={onOpen}
                     />
-                    <p className="text-gray-500 pr-2">■ این محصول تنوع رنگ‌بندی و سایزبندی و ... دارد؟ از این بخش می‌تونید آنها را اضافه کنید.</p>
+                    {
+                        attributes.length
+                            ?
+                            <SubAttributeBox
+                                titleCard={attributes[0].attr.label}
+                                onHandleSubmit={() => { }}
+                            />
+                            : <p className="text-gray-500 pr-2">■ این محصول تنوع رنگ‌بندی و سایزبندی و ... دارد؟ از این بخش می‌تونید آنها را اضافه کنید.</p>
+                    }
                 </CardBody>
             </Card>
 
