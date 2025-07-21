d90792252106b84638cc98817e661fbf05fd7024
mh.kadem <khadem5939@gmail.com>
2025-07-21 11:15:36 +0330

    Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  //output: "export",
   reactStrictMode: false,
   env: {
     NEXT_PUBLIC_BASE_URL: "http://172.18.100.42:3001/api",
