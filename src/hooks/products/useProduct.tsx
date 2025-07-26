c765a38fa79963c0389f2c9e45bd28c0dadc1229
mh.kadem <khadem5939@gmail.com>
2025-07-26 12:49:20 +0330

    Update

--- src/hooks/products/useProduct.tsx
+++ src/hooks/products/useProduct.tsx
@@ -1,31 +1,33 @@
-import { Product } from "@/components/Admin/_products/__create/product-type"
-import { fetcher } from "@/utils/fetcher"
-import { useMutation } from "@tanstack/react-query"
+import { Product } from "@/components/Admin/_products/__create/product-type";
+import { fetcher } from "@/utils/fetcher";
+import { useMutation } from "@tanstack/react-query";
 
 export const useProductUpload = () => {
-    return useMutation({
-        mutationFn: (data: FormData) => {
-            return fetcher({
-                route: "/product/upload",
-                method: "POST",
-                body: data,
-                successText: "فایل ها با موفقیت بارگذاری شدند",
-                loadingText: "در حال بارگذاری فایل ها",
-            })
-        }
-    })
-}
+  return useMutation({
+    mutationFn: (data: FormData) => {
+      return fetcher({
+        route: "/product/upload",
+        method: "POST",
+        body: data,
+        successText: "فایل ها با موفقیت بارگذاری شدند",
+        loadingText: "در حال بارگذاری فایل ها",
+      });
+    },
+  });
+};
 
 export const useProductCreate = () => {
-    return useMutation({
-        mutationFn: (data: Product) => {
-            return fetcher({
-                route: "/product",
-                method: "POST",
-                body: data,
-                successText: "محصول با موفقیت ایجاد شد. لطفا ادامه پروسه رو دنبال کنید تا محصول نهایی شود",
-                loadingText: "در حال ایجاد محصول",
-            })
-        }
-    })
-}
+  return useMutation({
+    mutationFn: (data: Product) => {
+      return fetcher({
+        route: "/product",
+        method: "POST",
+        body: data,
+        isActiveToast: true,
+        successText:
+          "محصول با موفقیت ایجاد شد. لطفا ادامه پروسه رو دنبال کنید تا محصول نهایی شود",
+        loadingText: "در حال ایجاد محصول",
+      });
+    },
+  });
+};
