cb52b879c2e55c2e670adb09bfe1f11394544c32
mh.kadem <khadem5939@gmail.com>
2025-09-28 11:36:22 +0330

    Create new component CardContent

--- src/app/admin/products/brands/page.tsx
+++ src/app/admin/products/brands/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import BoxHeader from "@/components/Admin/_products/__create/helpers/BoxHeader";
-import HeaderAction from "@/components/Admin/_products/__create/helpers/HeaderAction";
 import SearchInput from "@/components/Admin/_products/__create/helpers/SearchInput";
+import CardContent from "@/components/Admin/CardContent";
 import OptionBox from "@/components/Admin/OptionBox";
-import AppPagination from "@/components/Helper/AppPagination";
 import LoadingApiCall from "@/components/Helper/LoadingApiCall";
 import { useGetBrands } from "@/hooks/useBrand";
-import { Button, Card, CardBody, CardFooter, Image } from "@heroui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Tooltip,
+} from "@heroui/react";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 import { BiSortAlt2 } from "react-icons/bi";
+import { HiOutlineSquaresPlus } from "react-icons/hi2";
 import { IoFilter } from "react-icons/io5";
 import { LuBox } from "react-icons/lu";
 import { TbBrandArc } from "react-icons/tb";
@@ -54,56 +61,38 @@ const BrandsProduct = () => {
           </section>
         </CardBody>
       </Card>
-      <Card>
-        <BoxHeader
-          title="برندها"
-          color="text-white bg-gradient-to-r from-orange-500 via-purple-500 to-slate-700"
-          icon={<TbBrandArc className="text-3xl" />}
-        />
-        <CardBody>
-          <div className="border-b pb-3 mb-6">
-            <HeaderAction
-              title="لیست برندها"
-              textBtn="+ برند جدید"
-              onPress={() => {}}
-            />
-          </div>
-
-          {isLoading ? (
-            <LoadingApiCall />
-          ) : brands?.data?.items?.length ? (
-            <div className="grid grid-cols-3 gap-2 space-y-4">
-              {brands.data.items.map((b: any) => {
-                return (
-                  <Card key={b.id} isPressable shadow="sm" onPress={() => {}}>
-                    <CardBody className="overflow-visible p-0">
-                      <Image
-                        alt={b.title}
-                        className="w-full object-cover h-[140px]"
-                        radius="lg"
-                        shadow="sm"
-                        src={b.logo}
-                        width="100%"
-                      />
-                    </CardBody>
-                    <CardFooter className="text-small justify-between">
-                      <b>{b.name}</b>
-                      <p className="text-default-500">{b.slug}</p>
-                    </CardFooter>
-                  </Card>
-                );
-              })}
-            </div>
-          ) : (
-            <p className="text-center py-6">
-              {searchInp?.length
-                ? "برای سرچ شما برندی وجود ندارد"
-                : "فعلا برندی وجود ندارد"}
-            </p>
-          )}
-        </CardBody>
-      </Card>
-      <AppPagination meta={brands?.data.meta} />
+      <CardContent
+        datas={brands}
+        isExistItems={brands?.data?.items?.length}
+        isLoading={isLoading}
+        title="لیست برندها"
+        keyTitle="برند"
+        onClicked={() => {}}
+        icon={<TbBrandArc className="text-3xl animate-pulse" />}
+        searchInp={!!searchInp?.length}
+        styleContent="grid grid-cols-3 gap-4"
+      >
+        {brands?.data?.items?.map((b: any) => {
+          return (
+            <Card key={b.id} isPressable shadow="sm" onPress={() => {}}>
+              <CardBody className="overflow-visible p-0">
+                <Image
+                  alt={b.title}
+                  className="w-full object-cover h-[140px]"
+                  radius="lg"
+                  shadow="sm"
+                  src={b.logo}
+                  width="100%"
+                />
+              </CardBody>
+              <CardFooter className="text-small justify-between">
+                <b>{b.name}</b>
+                <p className="text-default-500">{b.slug}</p>
+              </CardFooter>
+            </Card>
+          );
+        })}
+      </CardContent>
     </section>
   );
 };
---
+++ src/components/Admin/CardContent.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Card, CardBody, Tooltip } from "@heroui/react";
+import { HiOutlineSquaresPlus } from "react-icons/hi2";
+import LoadingApiCall from "../Helper/LoadingApiCall";
+import AppPagination from "../Helper/AppPagination";
+
+type CardContentProps = {
+  icon?: any;
+  title: string;
+  keyTitle: string;
+  onClicked: () => void;
+  isLoading: boolean;
+  datas: any;
+  header?: any;
+  children: React.ReactNode;
+  searchInp?: boolean;
+  styleContent?: string;
+  isExistItems: boolean;
+};
+
+const CardContent: React.FC<CardContentProps> = ({
+  keyTitle,
+  title,
+  icon,
+  onClicked,
+  isLoading,
+  datas,
+  header,
+  children,
+  searchInp,
+  styleContent,
+  isExistItems,
+}) => {
+  return (
+    <section className="flex flex-col gap-6">
+      {header}
+      <Card>
+        <CardBody className="overflow-hidden">
+          <div className="w-full flex items-center border-b p-3 animate-appearance-in bg-gray-50 rounded-xl mb-6 justify-between">
+            <div className="flex items-center gap-2">
+              {icon}
+              <p className="font-normal text-[15px] text-gray-600">{title}</p>
+            </div>
+
+            <Tooltip
+              color="foreground"
+              content={`افزودن ${keyTitle} جدید`}
+              placement="right"
+              closeDelay={1000}
+              showArrow
+            >
+              <div
+                className="bg-gray-200 rounded-md p-1.5 cursor-pointer"
+                onClick={onClicked}
+              >
+                <HiOutlineSquaresPlus className="text-2xl" />
+              </div>
+            </Tooltip>
+          </div>
+
+          {isLoading ? (
+            <LoadingApiCall />
+          ) : isExistItems ? (
+            <div className={styleContent}>{children}</div>
+          ) : (
+            <p className="text-center py-6">
+              {searchInp ? "سرچ شمل نتیجه ای نداشت" : "هنوز آیتمی ساخته نشده!!"}
+            </p>
+          )}
+        </CardBody>
+      </Card>
+      <AppPagination meta={datas?.data?.meta} />
+    </section>
+  );
+};
+
+export default CardContent;
