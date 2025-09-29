6b1e8221a71cfbf063dedd9400bdeaf9f00a215e
mh.kadem <khadem5939@gmail.com>
2025-09-29 10:48:29 +0330

    Update and seprate Filter from BrandsProduct

--- src/app/admin/products/brands/page.tsx
+++ src/app/admin/products/brands/page.tsx
@@ -2,23 +2,17 @@
 
 import AddNewBrandModal from "@/components/Admin/_products/__brands/AddNewBrandModal";
 import BrandCard from "@/components/Admin/_products/__brands/BrandCard";
-import BoxHeader from "@/components/Admin/_products/__create/helpers/BoxHeader";
-import SearchInput from "@/components/Admin/_products/__create/helpers/SearchInput";
+import BrandFilters from "@/components/Admin/_products/__brands/BrandFilters";
 import CardContent from "@/components/Admin/CardContent";
-import OptionBox from "@/components/Admin/OptionBox";
 import DynamicModal from "@/components/Helper/DynamicModal";
 import { useDeleteBrand, useGetBrands } from "@/hooks/useBrand";
-import { Card, CardBody, useDisclosure } from "@heroui/react";
+import { useDisclosure } from "@heroui/react";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
-import { BiSortAlt2 } from "react-icons/bi";
-import { IoFilter } from "react-icons/io5";
-import { LuBox } from "react-icons/lu";
-import { TbBrandArc, TbEdit } from "react-icons/tb";
+import { TbBrandArc } from "react-icons/tb";
 
 const BrandsProduct = () => {
   const searchParams = useSearchParams();
-  const [searchInp, setSearchInp] = useState<string | undefined>(undefined);
   const [editBrand, setEditBrand] = useState<any>(null);
   const [deleteBrandId, setDeleteBrandId] = useState<number | null>(null);
   const { data: brands, isLoading } = useGetBrands(
@@ -40,33 +34,7 @@ const BrandsProduct = () => {
   return (
     <>
       <section className="flex flex-col gap-6">
-        <Card className="shadow-none">
-          <BoxHeader
-            title="باکس فیلتر"
-            color="text-white bg-gray-800"
-            icon={<LuBox className="text-3xl" />}
-          />
-          <CardBody className="flex flex-col gap-4">
-            <section className="w-full">
-              <SearchInput
-                placeholder="جستجو در برندها..."
-                onSearch={setSearchInp}
-              />
-            </section>
-            <section className="flex flex-wrap items-center gap-2 justify-between">
-              <OptionBox
-                title="فیلتر"
-                icon={<IoFilter className="!text-[16px]" />}
-                onClick={() => {}}
-              />
-              <OptionBox
-                title="مرتب سازی"
-                icon={<BiSortAlt2 className="!text-[16px]" />}
-                onClick={() => {}}
-              />
-            </section>
-          </CardBody>
-        </Card>
+        <BrandFilters />
         <CardContent
           datas={brands}
           isExistItems={brands?.data?.items?.length}
@@ -75,7 +43,7 @@ const BrandsProduct = () => {
           keyTitle="برند"
           onAdd={onOpenBrandModal}
           icon={<TbBrandArc className="text-3xl animate-pulse" />}
-          searchInp={!!searchInp?.length}
+          //searchInp={!!searchInp?.length}
         >
           <div className="flex flex-wrap justify-center gap-4">
             {brands?.data?.items?.map((b: any) => {
---
+++ src/components/Admin/_products/__brands/BrandFilters.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Card, CardBody } from "@heroui/react";
+import BoxHeader from "../__create/helpers/BoxHeader";
+import { LuBox } from "react-icons/lu";
+import SearchInput from "../__create/helpers/SearchInput";
+import OptionBox from "../../OptionBox";
+import { BiSortAlt2 } from "react-icons/bi";
+import { IoFilter } from "react-icons/io5";
+import { useState } from "react";
+
+const BrandFilters = () => {
+  const [searchInp, setSearchInp] = useState<string | undefined>(undefined);
+
+  return (
+    <Card className="shadow-none">
+      <BoxHeader
+        title="باکس فیلتر"
+        color="text-white bg-gray-800"
+        icon={<LuBox className="text-3xl" />}
+      />
+      <CardBody className="flex flex-col gap-4">
+        <section className="w-full">
+          <SearchInput
+            placeholder="جستجو در برندها..."
+            onSearch={setSearchInp}
+          />
+        </section>
+        <section className="flex flex-wrap items-center gap-2 justify-between">
+          <OptionBox
+            title="فیلتر"
+            icon={<IoFilter className="!text-[16px]" />}
+            onClick={() => {}}
+          />
+          <OptionBox
+            title="مرتب سازی"
+            icon={<BiSortAlt2 className="!text-[16px]" />}
+            onClick={() => {}}
+          />
+        </section>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default BrandFilters;
