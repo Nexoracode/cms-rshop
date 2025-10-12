152b1addee252c9c068097c8a0094dbbeb87ae70
mh.kadem <khadem5939@gmail.com>
2025-10-12 11:00:38 +0330

    Upadte Filter and Sorting Coupons Modal

--- src/components/Admin/_store/__promotions/DiscountCode/CouponsFilter.tsx
+++ src/components/Admin/_store/__promotions/DiscountCode/CouponsFilter.tsx
@@ -1,43 +1,57 @@
 "use client";
 
-import { Button, Card, CardBody } from "@heroui/react";
-import BoxHeader from "@/components/Admin/_products/__create/helpers/BoxHeader";
-import { GoArrowUpRight } from "react-icons/go";
-import Link from "next/link";
-import { LuSettings2, LuTicketPercent } from "react-icons/lu";
+import { Card, CardBody, useDisclosure } from "@heroui/react";
+import DebouncedSearchURL from "@/components/Helper/DebouncedSearchInput";
+import OptionBox from "@/components/Admin/OptionBox";
+import { IoFilter } from "react-icons/io5";
+import { BiSortAlt2 } from "react-icons/bi";
+import SortingCouponsModal from "./SortingCouponsModal";
+import FilterCouponsModal from "./FilterCouponsModal";
 
 const CouponsFilter = () => {
+  const {
+    isOpen: isSortOpen,
+    onOpen: onOpenSort,
+    onOpenChange: onSortOpenChange,
+  } = useDisclosure();
+
+  const {
+    isOpen: isFilterOpen,
+    onOpen: onOpenFilter,
+    onOpenChange: onFilterOpenChange,
+  } = useDisclosure();
+
   return (
     <>
       <Card className="shadow-md">
         <CardBody className="flex flex-col gap-4">
-          <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4 text-start bg-slate-50 rounded-xl p-2">
-            <p className="pr-2">صفحات مرتبط</p>
-            <div className="flex flex-wrap xs:flex-nowrap gap-2 w-full sm:w-fit">
-              <Button
-                className="pl-5 w-full sm:w-fit"
-                variant="flat"
-                size="sm"
-                as={Link}
-                href={"/admin/store/coupons/create"}
-              >
-                <GoArrowUpRight className="text-xl" />
-                ایجاد کد تخفیف
-              </Button>
-              <Button
-                className="pl-5 w-full sm:w-fit"
-                variant="flat"
-                size="sm"
-                as={Link}
-                href={"/admin/settings/discounts"}
-              >
-                <GoArrowUpRight className="text-xl" />
-                تنظیمات تخفیف
-              </Button>
+          <div className="flex flex-col sm:flex-row items-center gap-2 bg-slate-50 rounded-xl p-2">
+            <DebouncedSearchURL placeholder="جستجو در کد تخفیف ها..." />
+            <div className="w-full sm:w-fit flex items-center gap-2">
+              <OptionBox
+                title="فیلتر"
+                icon={<IoFilter className="!text-[16px]" />}
+                onClick={onOpenFilter}
+                style="w-full sm:w-fit"
+              />
+              <OptionBox
+                title="مرتب سازی"
+                icon={<BiSortAlt2 className="!text-[16px]" />}
+                onClick={onOpenSort}
+                style="w-full sm:w-fit"
+              />
             </div>
           </div>
         </CardBody>
       </Card>
+      <SortingCouponsModal
+        isOpen={isSortOpen}
+        onOpenChange={onSortOpenChange}
+      />
+      <FilterCouponsModal
+        isOpen={isFilterOpen}
+        onOpenChange={onFilterOpenChange}
+      />
     </>
   );
 };
---
+++ src/components/Admin/_store/__promotions/DiscountCode/FilterCouponsModal.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+type Props = { isOpen: boolean; onOpenChange: () => void };
+
+const FilterCouponsModal: React.FC<Props> = ({ isOpen, onOpenChange }) => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default FilterCouponsModal;
