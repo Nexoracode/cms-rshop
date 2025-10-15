d6173446fa19f56b32c52881b397fe1fa992ada5
mh.kadem <khadem5939@gmail.com>
2025-10-15 12:09:20 +0330

    Update

--- src/components/Admin/_products/ProductWithVariantsBox.tsx
+++ src/components/Admin/_products/ProductWithVariantsBox.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineCategory } from "react-icons/md";
 import SelectableCard from "@/components/common/SelectionBox/SelectableCard";
 import { RiDiscountPercentLine } from "react-icons/ri";
 
 type Props = {
   product: any;
-  onSelect?: (id: number, selected: boolean, product?: any) => void;
+  onSelect?: (
+    items: { product_id: number; variant_id: number | null }[]
+  ) => void;
   selectedIds?: number[];
 };
 
@@ -16,102 +18,143 @@ const ProductWithVariantsBox: React.FC<Props> = ({
   onSelect,
   selectedIds = [],
 }) => {
-  const id = product.id;
+  const [productSelected, setProductSelected] = useState(false);
+  const [selectedVariants, setSelectedVariants] = useState<number[]>([]);
+
+  const handleProductSelect = (selected: boolean) => {
+    setProductSelected(selected);
+    if (selected) {
+      setSelectedVariants([]);
+      onSelect?.([{ product_id: product.id, variant_id: null }]);
+    } else {
+      onSelect?.([]);
+    }
+  };
+
+  const handleVariantSelect = (variantId: number, selected: boolean) => {
+    let updatedVariants = [...selectedVariants];
+    if (selected) updatedVariants.push(variantId);
+    else updatedVariants = updatedVariants.filter((id) => id !== variantId);
+
+    setSelectedVariants(updatedVariants);
+
+    if (updatedVariants.length > 0) setProductSelected(false);
+
+    const output =
+      updatedVariants.length > 0
+        ? updatedVariants.map((id) => ({
+            product_id: product.id,
+            variant_id: id,
+          }))
+        : [];
+
+    onSelect?.(output);
+  };
 
   return (
-    <>
-      <SelectableCard
-        id={id}
-        selectedIds={selectedIds}
-        disabled={false}
-        onSelectionChange={(idVal, sel) =>
-          onSelect?.(idVal as number, sel, product)
-        }
-        className="max-w-[300px] w-full sm:max-w-full"
-      >
-        <div className="flex flex-col items-center sm:flex-row gap-4 text-start">
-          <div className="relative w-fit h-full">
-            <img
-              alt="product cover"
-              className="object-cover w-full sm:w-[130px] h-[188px] sm:h-[110px] rounded-xl"
-              src={product.media_pinned?.url ?? product.image}
-            />
-            {!product.is_visible && (
-              <div className="absolute inset-0 text-center flex items-center justify-center text-lg px-3 py-1 bg-gray-600/60 text-white shadow-lg rounded-lg">
-                <p className="animate-bounce">عدم نمایش</p>
-              </div>
-            )}
+    <SelectableCard
+      id={product.id}
+      selectedIds={productSelected ? [product.id] : []}
+      disabled={selectedVariants.length > 0}
+      onSelectionChange={(idVal, sel) => handleProductSelect(sel)}
+      className="max-w-[300px] w-full sm:max-w-full"
+    >
+      {/* Product info */}
+      <div className="flex flex-col items-center sm:flex-row gap-4 text-start">
+        <div className="relative w-fit h-full">
+          <img
+            alt="product cover"
+            className="object-cover w-full sm:w-[130px] h-[188px] sm:h-[110px] rounded-xl"
+            src={product.media_pinned?.url ?? product.image}
+          />
+          {!product.is_visible && (
+            <div className="absolute inset-0 text-center flex items-center justify-center text-lg px-3 py-1 bg-gray-600/60 text-white shadow-lg rounded-lg">
+              <p className="animate-bounce">عدم نمایش</p>
+            </div>
+          )}
+        </div>
+
+        <div className="w-full sm:h-[110px] flex flex-col justify-between pr-0 sm:p-2 gap-4">
+          <div className="flex flex-col gap-3 sm:flex-row justify-between items-center w-full">
+            <div className="text-[15px] text-black/80 flex flex-col sm:flex-row items-center gap-1">
+              <p className="truncate max-w-[220px] sm:max-w-[240px]">
+                {product.name ?? product.title}
+              </p>
+              <span className="text-gray-600 text-xs">
+                ({product.category?.title})
+              </span>
+            </div>
           </div>
 
-          <div className="w-full sm:h-[110px] flex flex-col justify-between pr-0 sm:p-2 gap-4">
-            <div className="flex flex-col gap-3 sm:flex-row justify-between items-center w-full">
-              <div className="text-[15px] text-black/80 flex flex-col sm:flex-row items-center gap-1">
-                <p className="truncate max-w-[220px] sm:max-w-[240px]">
-                  {product.name ?? product.title}
-                </p>
-                <span className="text-gray-600 text-xs">
-                  ({product.category?.title})
-                </span>
-              </div>
+          <div className="flex items-end justify-between">
+            <div className="flex flex-col gap-2 cursor-auto">
+              <p className="text-gray-600 text-[13px]">
+                موجودی{" "}
+                {product.is_limited_stock
+                  ? "نامحدود"
+                  : product.stock === 0
+                  ? "ندارد"
+                  : `${product.stock} عدد`}
+              </p>
             </div>
 
-            <div className="flex items-end justify-between">
-              <div className="flex flex-col gap-2 cursor-auto">
-                <p className="text-gray-600 text-[13px]">
-                  موجودی{" "}
-                  {product.is_limited_stock
-                    ? "نامحدود"
-                    : product.stock === 0
-                    ? "ندارد"
-                    : `${product.stock} عدد`}
-                </p>
-              </div>
-
-              <div className="flex items-end">
-                <div className="text-gray-600">
-                  {product.discount_amount > 0 ||
-                  product.discount_percent > 0 ? (
-                    <div className="flex flex-col items-end gap-2 sm:gap-1">
-                      <div className="flex items-center gap-1">
-                        <span className="text-xs text-gray-500 line-through decoration-2 decoration-gray-400">
-                          {Number(product.price).toLocaleString("fa-IR")}
-                        </span>
-                        <span>تومان</span>
-                      </div>
-                      <span className="text-[15px] text-gray-800">
-                        {Number(
-                          Math.max(
-                            0,
-                            product.price -
-                              (product.discount_amount > 0
-                                ? product.discount_amount
-                                : (product.discount_percent / 100) *
-                                  product.price)
-                          )
-                        ).toLocaleString("fa-IR")}{" "}
-                        تومان
+            <div className="flex items-end">
+              <div className="text-gray-600">
+                {product.discount_amount > 0 || product.discount_percent > 0 ? (
+                  <div className="flex flex-col items-end gap-2 sm:gap-1">
+                    <div className="flex items-center gap-1">
+                      <span className="text-xs text-gray-500 line-through decoration-2 decoration-gray-400">
+                        {Number(product.price).toLocaleString("fa-IR")}
                       </span>
+                      <span>تومان</span>
                     </div>
-                  ) : (
                     <span className="text-[15px] text-gray-800">
-                      {Number(product.price).toLocaleString("fa-IR")} تومان
+                      {Number(
+                        Math.max(
+                          0,
+                          product.price -
+                            (product.discount_amount > 0
+                              ? product.discount_amount
+                              : (product.discount_percent / 100) *
+                                product.price)
+                        )
+                      ).toLocaleString("fa-IR")}{" "}
+                      تومان
                     </span>
-                  )}
-                </div>
+                  </div>
+                ) : (
+                  <span className="text-[15px] text-gray-800">
+                    {Number(product.price).toLocaleString("fa-IR")} تومان
+                  </span>
+                )}
               </div>
             </div>
           </div>
         </div>
-        {product.variants?.length > 0 ? (
-          <div className="flex flex-col gap-2 border-t pt-2 mt-3 mx-4">
-            <div className="flex items-center justify-between mb-3">
-              <p className="text-gray-600">تنوع محصول ها</p>
-              <MdOutlineCategory className="text-purple-500 text-xl" />
-            </div>
-            {product.variants.map((variant: any) => (
+      </div>
+
+      {/* Variants */}
+      {product.variants?.length > 0 && (
+        <div className="flex flex-col gap-2 border-t pt-2 mt-3 mx-4">
+          <div className="flex items-center justify-between mb-3">
+            <p className="text-gray-600">تنوع محصول ها</p>
+            <MdOutlineCategory className="text-purple-500 text-xl" />
+          </div>
+          {product.variants.map((variant: any) => (
+            <SelectableCard
+              key={variant.id}
+              id={variant.id}
+              selectedIds={selectedVariants}
+              disabled={productSelected}
+              onSelectionChange={(idVal, sel) =>
+                handleVariantSelect(variant.id, sel)
+              }
+              className="shadow-none border-none rounded-xl hover:shadow-none"
+              bodyClassName="p-0 shadow-none hover:shadow-none"
+            >
               <div
                 key={variant.id}
-                className="flex flex-wrap sm:flex-nowrap items-center justify-between rounded-xl py-3 px-4 bg-slate-50 border border-transparent hover:border hover:border-gray-300 transition-all duration-500"
+                className="flex flex-wrap sm:flex-nowrap items-center justify-between py-3 px-4 rounded-xl bg-slate-50 border border-transparent hover:border hover:border-gray-300 transition-all duration-300"
               >
                 <div className="flex flex-wrap gap-2 text-sm text-gray-700">
                   {variant.name}
@@ -145,18 +188,11 @@ const ProductWithVariantsBox: React.FC<Props> = ({
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-        ) : (
-           <div className="flex flex-col gap-2 border-t pt-2 mt-3 mx-4">
-            <div className="flex items-center justify-between">
-              <p className="text-gray-600">تنوع محصولی ندارد</p>
-              <MdOutlineCategory className="text-purple-500 text-xl" />
-            </div>
-          </div>
-        )}
-      </SelectableCard>
-    </>
+            </SelectableCard>
+          ))}
+        </div>
+      )}
+    </SelectableCard>
   );
 };
 
