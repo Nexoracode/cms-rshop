4c2c6399e20ad4a058d81aadff7cb02ef70905c7
mh.kadem <khadem5939@gmail.com>
2025-10-20 12:46:27 +0330

    Create New component OptionButton

---
+++ src/components/ui/buttons/OptionButton.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@heroui/react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {
+  icon?: React.ReactNode;
+  title: string;
+  href?: string;
+  onClick?: () => void;
+  className?: string;
+  size?: "sm" | "md" | "lg";
+  variant?:
+    | "bordered"
+    | "faded"
+    | "flat"
+    | "ghost"
+    | "light"
+    | "shadow"
+    | "solid";
+};
+
+const OptionButton: React.FC<Props> = ({
+  icon,
+  title,
+  href,
+  onClick,
+  className = "",
+  size = "sm",
+  variant = "flat",
+}) => {
+  const content = (
+    <div className="flex items-center gap-2">
+      {icon && <span className="text-xl">{icon}</span>}
+      <span>{title}</span>
+    </div>
+  );
+
+  return href ? (
+    <Button
+      as={Link}
+      href={href}
+      variant={variant}
+      size={size}
+      className={`pl-5 ${className}`}
+    >
+      {content}
+    </Button>
+  ) : (
+    <Button
+      onPress={onClick}
+      variant={variant}
+      size={size}
+      className={`pl-5 ${className}`}
+    >
+      {content}
+    </Button>
+  );
+};
+
+export default OptionButton;
