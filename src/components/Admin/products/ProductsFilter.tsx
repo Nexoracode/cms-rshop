e2592cd27c32d88b6f0084e06f62ddbfc8003f47
mh.kadem <khadem5939@gmail.com>
2025-10-20 12:57:23 +0330

    Update ProductFilter

--- src/components/Admin/products/ProductsFilter.tsx
+++ src/components/Admin/products/ProductsFilter.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button, Card, CardBody, useDisclosure } from "@heroui/react";
-import OptionBox from "../OptionBox";
 import { BiSortAlt2 } from "react-icons/bi";
 import SortingModal from "./modals/SortingModal";
 import FilterModal from "./modals/FilterModal";
@@ -9,6 +8,7 @@ import { IoFilter } from "react-icons/io5";
 import DebouncedSearchURL from "@/components/widgets/DebouncedSearchInput";
 import Link from "next/link";
 import { GoArrowUpRight } from "react-icons/go";
+import OptionButton from "@/components/ui/buttons/OptionButton";
 
 const ProductsFilter = () => {
   const {
@@ -29,55 +29,41 @@ const ProductsFilter = () => {
         <CardBody className="flex flex-col gap-4">
           <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4 text-start bg-slate-50 rounded-xl p-2">
             <p className="pr-2">صفحات مرتبط</p>
-            <div className="flex flex-wrap xs:flex-nowrap gap-2 w-full sm:w-fit">
-              <Button
+            <div className="flex flex-wrap xs:flex-nowrap gap-2 w-full sm:w-fit">              
+              <OptionButton
+                title="ویژگی ها"
+                icon={<GoArrowUpRight className="text-xl" />}
+                href="/admin/products/variants"
                 className="pl-5 w-full sm:w-fit"
-                variant="flat"
-                size="sm"
-                as={Link}
-                href={"/admin/products/variants"}
-              >
-                <GoArrowUpRight className="text-xl" />
-                ویژگی ها
-              </Button>
-
-              <Button
+              />
+              <OptionButton
+                title="دسته بندی ها"
+                icon={<GoArrowUpRight className="text-xl" />}
+                href="/admin/products/categories"
                 className="pl-5 w-full sm:w-fit"
-                variant="flat"
-                size="sm"
-                as={Link}
-                href={"/admin/products/categories"}
-              >
-                <GoArrowUpRight className="text-xl" />
-                دسته بندی ها
-              </Button>
-
-              <Button
+              />
+              <OptionButton
+                title="برندها"
+                icon={<GoArrowUpRight className="text-xl" />}
+                href="/admin/products/brands"
                 className="pl-5 w-full sm:w-fit"
-                variant="flat"
-                size="sm"
-                as={Link}
-                href={"/admin/products/brands"}
-              >
-                <GoArrowUpRight className="text-xl" />
-                برندها
-              </Button>
+              />
             </div>
           </div>
           <div className="flex flex-col sm:flex-row items-center gap-2 bg-slate-50 rounded-xl p-2">
             <DebouncedSearchURL placeholder="جستجو در محصولات..." />
             <div className="w-full sm:w-fit flex items-center gap-2">
-              <OptionBox
+              <OptionButton
                 title="فیلتر"
                 icon={<IoFilter className="!text-[16px]" />}
                 onClick={onOpenFilter}
-                style="w-full sm:w-fit"
+                className="w-full sm:w-fit text-sky-600 bg-sky-100"
               />
-              <OptionBox
+              <OptionButton
                 title="مرتب سازی"
                 icon={<BiSortAlt2 className="!text-[16px]" />}
                 onClick={onOpenSort}
-                style="w-full sm:w-fit"
+                className="w-full sm:w-fit text-sky-600 bg-sky-100"
               />
             </div>
           </div>
