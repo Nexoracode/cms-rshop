8c0013140ba8d35b355249ba97245ba73f824987
mh.kadem <khadem5939@gmail.com>
2025-11-08 14:33:56 +0330

    Replace and modified ManualOrderPage

--- src/app/admin/orders/manual-order/page.tsx
+++ src/app/admin/orders/manual-order/page.tsx
@@ -1,56 +1,20 @@
 "use client";
 
-import { useState } from "react";
-import SwitchWrapper from "@/components/shared/SwitchWrapper";
-import { Button, Card, CardBody, CardFooter } from "@heroui/react";
-import { TbShoppingCartPlus } from "react-icons/tb";
-import DiscountInput from "@/components/forms/Inputs/DiscountInput";
-import { Discount } from "@/core/types";
+import ManualOrderForm from "@/components/features/orders/ManualOrderForm";
+import { ProductsSelectionProvider } from "@/components/features/products/SelectableProduct/ProductsSelectionContext";
+import { CustomersSelectionProvider } from "@/components/features/store/customers/SelectableCustomersBox/CustomersSelectionContext";
 
 const ManualOrder = () => {
-  const [discountValue, setDiscountValue] = useState(0);
-  const [discountType, setDiscountType] = useState<Discount>("percent");
-
-  const onDiscountChange = (type: Discount, value: number) => {
-    console.log("Discount changed:", type, value);
+  const handleReset = () => {
+    //setCustomers([]);
   };
 
   return (
-    <>
-      <Card className="shadow-md mt-6">
-      {/*   <BoxHeader
-          title="ثبت سفارش"
-          color="text-blue-700 bg-blue-700/10"
-          icon={<TbShoppingCartPlus className="text-3xl" />}
-        /> */}
-        <CardBody className="text-right flex flex-col gap-6">
-          <SwitchWrapper
-            label="تخفیف فاکتور"
-            description="این مبلغ به عنوان تخفیف از مجموع فاکتور کسر می‌شود"
-          >
-            <DiscountInput
-              value={discountValue}
-              onValueChange={(val) => {
-                const v = val ?? 0;
-                setDiscountValue(v);
-                onDiscountChange(discountType, v);
-              }}
-              selectedKey={discountType}
-              onSelectChange={(val) => {
-                const t = val as Discount;
-                setDiscountType(t);
-                onDiscountChange(t, discountValue);
-              }}
-            />
-          </SwitchWrapper>
-        </CardBody>
-        <CardFooter className="w-full">
-          <Button variant="flat" color="secondary" className="w-full">
-            ثبت
-          </Button>
-        </CardFooter>
-      </Card>
-    </>
+    <ProductsSelectionProvider initialProducts={[]}>
+      <CustomersSelectionProvider initialCustomers={[]}>
+        <ManualOrderForm onReset={handleReset} />
+      </CustomersSelectionProvider>
+    </ProductsSelectionProvider>
   );
 };
 
---
+++ src/components/features/orders/ManualOrderForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import SwitchWrapper from "@/components/shared/SwitchWrapper";
+import DiscountInput from "@/components/forms/Inputs/DiscountInput";
+import { Discount } from "@/core/types";
+import BaseCard from "@/components/ui/BaseCard";
+import { HiOutlineDocumentText } from "react-icons/hi2";
+import FormActionButtons from "@/components/common/FormActionButtons";
+import SelectableUsersBox from "@/components/features/store/customers/SelectableCustomersBox/SelectableCustomersBox";
+import SelectableProductsBox from "@/components/features/products/SelectableProduct/SelectableProductsBox";
+
+type ManualOrderFormProps = {
+  onReset?: () => void;
+};
+
+const ManualOrderForm: React.FC<ManualOrderFormProps> = ({ onReset }) => {
+  const [discountValue, setDiscountValue] = useState(0);
+  const [discountType, setDiscountType] = useState<Discount>("percent");
+
+  const onDiscountChange = (type: Discount, value: number) => {
+    console.log("Discount changed:", type, value);
+  };
+
+  return (
+    <BaseCard
+      className="shadow-md mt-6"
+      CardHeaderProps={{
+        title: "ایجاد سفارش دستی",
+        icon: <HiOutlineDocumentText />,
+        showIconInActionSlot: true,
+      }}
+      wrapperContents
+    >
+      {/*  <SwitchWrapper
+        label="تخفیف فاکتور"
+        description="این مبلغ به عنوان تخفیف از مجموع فاکتور کسر می‌شود"
+      >
+        <DiscountInput
+          value={discountValue}
+          onValueChange={(val) => {
+            const v = val ?? 0;
+            setDiscountValue(v);
+            onDiscountChange(discountType, v);
+          }}
+          selectedKey={discountType}
+          onSelectChange={(val) => {
+            const t = val as Discount;
+            setDiscountType(t);
+            onDiscountChange(t, discountValue);
+          }}
+        />
+      </SwitchWrapper> */}
+
+      <SelectableUsersBox onChange={() => {}} />
+
+      <SelectableProductsBox onChange={() => {}} />
+
+      <FormActionButtons cancelHref="/admin/orders" onSubmit={() => {}} />
+    </BaseCard>
+  );
+};
+
+export default ManualOrderForm;
