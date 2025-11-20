4ef17f926778efaf6e2403267fe753c1f98dbb21
mh.kadem <khadem5939@gmail.com>
2025-11-20 13:30:42 +0330

    Update LayOut

--- src/app/admin/(settings)/layout.tsx
+++ src/app/admin/(settings)/layout.tsx
@@ -9,7 +9,7 @@ const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
       <ShopInfosCard />
       <div className="min-h-[69vh]">
         <Breadcrumbs />
-        <div className="mx-auto mb-32 lg:mb-6 flex flex-col gap-4 mt-4">
+        <div className="mx-auto mb-32 lg:mb-6 flex flex-col gap-2 mt-4">
           {children}
         </div>
       </div>
