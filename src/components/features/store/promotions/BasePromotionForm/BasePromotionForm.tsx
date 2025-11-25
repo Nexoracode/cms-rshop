e920716ac9d26aaa9ac59924cb0fe2af86b2e297
mh.kadem <khadem5939@gmail.com>
2025-11-25 14:52:15 +0330

    Update

--- src/components/features/store/promotions/BasePromotionForm/BasePromotionForm.tsx
+++ src/components/features/store/promotions/BasePromotionForm/BasePromotionForm.tsx
@@ -64,11 +64,17 @@ export function BasePromotionForm({
   resetSignal,
 }: BasePromotionFormProps) {
   const config: PromotionFormConfig = FORM_CONFIGS[formType];
-  const { form, errors, handleFieldChange, canSubmit, setForm, setHasSubmitted } =
-    useFormHandler<PromotionForm>(initialLocalForm, {
-      onValidate: (f) => validatePromotionForm(f),
-      runValidationOnChange: true,
-    });
+  const {
+    form,
+    errors,
+    handleFieldChange,
+    canSubmit,
+    setForm,
+    setHasSubmitted,
+  } = useFormHandler<PromotionForm>(initialLocalForm, {
+    onValidate: (f) => validatePromotionForm(f),
+    runValidationOnChange: true,
+  });
 
   useEffect(() => {
     if (initialData) {
@@ -88,32 +94,26 @@ export function BasePromotionForm({
 
     if (!form.name?.trim()) errs.name = "نام پروموشن الزامی است.";
 
-    if (config.code && !form.code?.trim()) {
-      errs.code = "کد تخفیف الزامی است.";
-    }
+    if (config.code && !form.code?.trim()) errs.code = "کد تخفیف الزامی است.";
 
     if (config.discount_fields) {
-      if (!form.percent_discount && !form.amount_discount) {
+      if (!form.percent_discount && !form.amount_discount)
         errs.discount = "حداقل یکی از درصد یا مبلغ تخفیف را وارد کنید.";
-      }
     }
 
     if (scope === "products" && config.scope.includes("product")) {
-      if (!form.allowed_products || form.allowed_products.length === 0) {
+      if (!form.allowed_products || form.allowed_products.length === 0)
         errs.allowed_products = "حداقل یک محصول باید انتخاب شود.";
-      }
     }
 
     if (scope === "categories" && config.scope.includes("category")) {
-      if (!form.allowed_categories || form.allowed_categories.length === 0) {
+      if (!form.allowed_categories || form.allowed_categories.length === 0)
         errs.allowed_categories = "حداقل یک دسته‌بندی باید انتخاب شود.";
-      }
     }
 
     if (scope === "customers" && config.scope.includes("user")) {
-      if (!form.allowed_users || form.allowed_users.length === 0) {
+      if (!form.allowed_users || form.allowed_users.length === 0)
         errs.allowed_users = "حداقل یک کاربر باید انتخاب شود.";
-      }
     }
 
     return errs;
@@ -121,7 +121,7 @@ export function BasePromotionForm({
 
   const handleSubmit = () => {
     console.log(errors);
-    
+
     if (!canSubmit()) return;
 
     const payload = mapLocalFormToAPI(form, formType);
