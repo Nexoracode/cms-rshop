b58f9ed7d69a535e3c1bd8c41a2f9c0df6aaf695
mh.kadem <khadem5939@gmail.com>
2025-12-15 14:21:16 +0330

    Upadte

--- src/app/admin/orders/order/page.tsx
+++ src/app/admin/orders/order/page.tsx
@@ -5,8 +5,8 @@ import { useGetOneOrder } from "@/core/hooks/api/orders/useOrder";
 import { IoReceiptOutline } from "react-icons/io5";
 import OrderWizard from "@/components/features/orders/OrderProccess/OrderWizard";
 import UnifiedCard from "@/components/common/Card/UnifiedCard";
-import { GoArrowUpRight } from "react-icons/go";
 import OrderProccessFilter from "@/components/features/orders/OrderProccess/OrderProccessFilter";
+import OrderFactor from "@/components/features/orders/OrderFactor";
 
 const OrderDetail = () => {
   const searchParams = useSearchParams();
@@ -20,15 +20,19 @@ const OrderDetail = () => {
 
   return (
     <UnifiedCard
-      searchFilter={<OrderProccessFilter customerId={orderData?.user?.id} customerName={`${orderData?.user?.last_name}`}/>}
+      searchFilter={
+        <OrderProccessFilter
+          customerId={orderData?.user?.id}
+          customerName={`${orderData?.user?.last_name}`}
+        />
+      }
       isLoading={isLoading}
       isExistItems={!!orderData}
       searchInp={false}
       headerProps={{
         title: orderData?.is_manual ? "مشخصات سفارش (دستی)" : "مشخصات سفارش",
         icon: <IoReceiptOutline className="text-2xl" />,
-        textBtn: "مشاهده فاکتور",
-        btnIcon: <GoArrowUpRight />,
+        children: <OrderFactor order={orderData}/>,
       }}
     >
       <OrderWizard order={orderData} />
--- src/components/features/orders/OrderFactor.tsx
+++ src/components/features/orders/OrderFactor.tsx
@@ -1,10 +1,28 @@
 "use client";
 
-const OrderFactor = () => {
+import BaseModal from "@/components/ui/modals/BaseModal";
+import React from "react";
+import { GoArrowUpRight } from "react-icons/go";
+import { IoReceiptOutline } from "react-icons/io5";
+
+type OrderFactorProps = {
+  order: any;
+};
+
+const OrderFactor: React.FC<OrderFactorProps> = ({ order }) => {
   return (
-    <div>
-      <p>e</p>
-    </div>
+    <BaseModal
+      triggerProps={{
+        title: "مشاهده فاکتور",
+        className: "bg-[var(--color-primary)] text-white",
+        icon: <GoArrowUpRight />,
+        variant: "flat",
+      }}
+      title="فاکتور سفارش"
+      confirmText="چاپ فاکتور"
+      onConfirm={() => {}}
+      icon={<IoReceiptOutline />}
+    ></BaseModal>
   );
 };
 
