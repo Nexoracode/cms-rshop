c2b999519a55df9abe15db1a898fc5a8ca621fb4
mh.kadem <khadem5939@gmail.com>
2025-12-15 13:21:08 +0330

    Update ConfirmingPaymentStep

--- src/components/features/orders/OrderProccess/OrderSteps/ConfirmingPaymentStep/ConfirmingPaymentStep.tsx
+++ src/components/features/orders/OrderProccess/OrderSteps/ConfirmingPaymentStep/ConfirmingPaymentStep.tsx
@@ -3,6 +3,7 @@ import { useReviewCardToCardPayment } from "@/core/hooks/api/card/useCard";
 import { useUpdateOrderStatus } from "@/core/hooks/api/orders/useOrder";
 import RejectReceiptImageModal from "./RejectReceiptImageModal";
 import Link from "next/link";
+import { Alert } from "@heroui/react";
 
 const ConfirmingPaymentStep = ({ order }: { order: any }) => {
   const reviewCardToCardPayment = useReviewCardToCardPayment();
@@ -20,37 +21,41 @@ const ConfirmingPaymentStep = ({ order }: { order: any }) => {
     updateOrderStatus.mutate({ id: order.id, status: "preparing" });
   };
 
-  return (
-    <>
-      <div className="text-center">
-        <p className="text-default-600 leading-7 mb-6">
-          مشتری پرداخت کارت به کارت انجام داده و تصویر رسید را ارسال کرده است.
-        </p>
+  return order.payment && order?.payment?.payment_method === "card_to_card" ? (
+    <div className="text-center">
+      <p className="text-default-600 leading-7 mb-6">
+        مشتری پرداخت کارت به کارت انجام داده و تصویر رسید را ارسال کرده است.
+      </p>
 
-        <Link
-          href={order?.payment?.receipt_image || "/images/placeholder.png"}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={order?.payment?.receipt_image || "/images/placeholder.png"}
-            alt="رسید پرداخت"
-            className="w-48 mx-auto mt-4 rounded-lg hover:scale-110 transition-all cursor-pointer duration-300 shadow-lg"
-          />
-        </Link>
+      <Link
+        href={order?.payment?.receipt_image || "/images/placeholder.png"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={order?.payment?.receipt_image || "/images/placeholder.png"}
+          alt="رسید پرداخت"
+          className="w-48 mx-auto mt-4 rounded-lg hover:scale-110 transition-all cursor-pointer duration-300 shadow-lg"
+        />
+      </Link>
 
-        <div className="mt-8 flex items-center justify-center">
-          <RejectReceiptImageModal
-            onConfirm={(admin_note) => rejectReceiptImage(admin_note)}
-          />
-          <FormActionButtons
-            onSubmit={acceptReceiptImage}
-            isSubmitting={false}
-            submitText="تأیید پرداخت"
-          />
-        </div>
+      <div className="mt-8 flex items-center justify-center">
+        <RejectReceiptImageModal
+          onConfirm={(admin_note) => rejectReceiptImage(admin_note)}
+        />
+        <FormActionButtons
+          onSubmit={acceptReceiptImage}
+          isSubmitting={false}
+          submitText="تأیید پرداخت"
+        />
       </div>
-    </>
+    </div>
+  ) : (
+    <Alert
+      color="secondary"
+      title="پرداخت در حال انجام"
+      description="پرداخت مشتری در حال پردازش است. لطفاً منتظر بمانید."
+    />
   );
 };
 
