b9bef99b31b394f4f2085b05cf74c4625a61bd4e
mh.kadem <khadem5939@gmail.com>
2025-12-25 14:18:44 +0330

    Update

--- src/components/features/store/(main-pages)/side-banner/SideBannerEmptyState.tsx
+++ src/components/features/store/(main-pages)/side-banner/SideBannerEmptyState.tsx
@@ -1,6 +1,6 @@
-import HeroSliderFormModal from "../hero-slider/HeroSliderFormModal";
 import AddSectionCard from "../shared/AddSectionCard";
 import { CiImageOn } from "react-icons/ci";
+import SideBannerFormModal from "./SideBannerFormModal";
 
 const SideBannerEmptyState = () => {
   return (
@@ -9,7 +9,7 @@ const SideBannerEmptyState = () => {
       children={
         <div className="flex flex-col gap-4 items-center">
           <CiImageOn className="text-gray-600 text-[70px]" />
-          <HeroSliderFormModal position="top_left" bannerId={1} defaultValues={[]} isOpen={false} onOpenChange={() => {}}/>
+          <SideBannerFormModal position="top_left" bannerId={1} defaultValues={[]} isOpen={false} onOpenChange={() => {}}/>
         </div>
       }
     />
