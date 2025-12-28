24a4a3a81c99b9e904a8906814d57935e4387ba8
mh.kadem <khadem5939@gmail.com>
2025-12-28 14:06:21 +0330

    Update SectionSliderContainer

--- src/components/features/store/(main-pages)/sections/SectionsSliderContainer.tsx
+++ src/components/features/store/(main-pages)/sections/SectionsSliderContainer.tsx
@@ -4,7 +4,7 @@ import AddNewCategorySection from "./AddNewSections/CategorySection/AddNewCatego
 import SectionTemplate from "./SectionTemplate";
 
 type Props = {
-  sections?: any[];
+  sections?: any;
 };
 
 const SectionsSliderContainer: React.FC<Props> = ({ sections = [] }) => {
@@ -12,11 +12,37 @@ const SectionsSliderContainer: React.FC<Props> = ({ sections = [] }) => {
     <div className="flex flex-col gap-10 justify-center items-center">
       <div className="w-full flex flex-col gap-8">
         {sections.length ? (
-          sections
-            .sort((a, b) => a.sort_order - b.sort_order)
-            .map((section) => (
-              <SectionTemplate key={section.id} section={section} />
-            ))
+          <>
+            {sections?.most_popular ? (
+              sections?.most_popular
+                .sort((a: any, b: any) => a.sort_order - b.sort_order)
+                .map((section: any) => (
+                  <SectionTemplate key={section.id} section={section} />
+                ))
+            ) : (
+              <AddNewPopularSection />
+            )}
+
+            {sections?.category_based ? (
+              sections?.category_based
+                .sort((a: any, b: any) => a.sort_order - b.sort_order)
+                .map((section: any) => (
+                  <SectionTemplate key={section.id} section={section} />
+                ))
+            ) : (
+              <AddNewCategorySection />
+            )}
+
+            {sections?.special_products ? (
+              sections?.special_products
+                .sort((a: any, b: any) => a.sort_order - b.sort_order)
+                .map((section: any) => (
+                  <SectionTemplate key={section.id} section={section} />
+                ))
+            ) : (
+              <AddNewProducsSection />
+            )}
+          </>
         ) : (
           <div className="flex flex-col gap-6">
             <AddNewPopularSection />
