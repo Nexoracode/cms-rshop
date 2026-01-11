94f417e0ceb6fb657ce4cb828f3ad1141fb35b2e
mh.kadem <khadem5939@gmail.com>
2026-01-11 13:36:56 +0330

    Design HeroBox for Drag and Drop

--- src/components/features/store/(main-pages)/hero-slider/HeroSliderLayoutModal.tsx
+++ src/components/features/store/(main-pages)/hero-slider/HeroSliderLayoutModal.tsx
@@ -6,6 +6,7 @@ import { TfiLayoutSlider } from "react-icons/tfi";
 import { ActionButton } from "@/components/ui/buttons/ActionButton";
 import { BiLayout } from "react-icons/bi";
 import { HeroSlider } from "./hero-slider.types";
+import Image from "next/image";
 
 type HeroSliderLayoutModalProps = {
   isOpen?: boolean;
@@ -36,7 +37,45 @@ const HeroSliderLayoutModal: React.FC<HeroSliderLayoutModalProps> = ({
       size="3xl"
       icon={<TfiLayoutSlider />}
       isActiveFooter={false}
-    ></BaseModal>
+    >
+      <div className="flex items-center gap-1 mx-auto -mb-8">
+        {sliders.map((slider, index) => {
+          const textColor = !slider.image_url
+            ? !slider.is_dark
+              ? "text-black"
+              : "text-white"
+            : "text-white";
+
+          return (
+            <div
+              key={index}
+              className={`hover-reveal-parent w-28 h-28 rounded select-none flex items-end cursor-pointer ${
+                !slider.image_url ? `rounded-2xl` : "bg-black/80"
+              }`}
+              style={{
+                backgroundColor: !slider.image_url
+                  ? slider.background_color || "gray"
+                  : "",
+              }}
+            >
+              <p className={`text-md truncate z-10 w-full pr-2 pb-1 ${textColor}`}>{slider.title}</p>
+
+              {slider.image_url ? (
+                <Image
+                  src={slider.image_url}
+                  alt={slider.title}
+                  fill
+                  priority
+                  className="object-cover absolute inset-0 z-0 opacity-40"
+                />
+              ) : (
+                ""
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </BaseModal>
   );
 };
 
