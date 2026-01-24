2e12b89b69dc0a0b9f5e8cfee8bfc21eb5ce64e0
mh.kadem <khadem5939@gmail.com>
2026-01-24 11:00:22 +0330

    Create midleware

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const protectedRoutes = ["/admin", "/factor"];
+const adminRoutes = ["/admin"];
+const authRoutes = ["/signin", "/"];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const token = request.cookies.get("access_token")?.value;
+  const refresh = request.cookies.get("refresh_token")?.value;
+  const role = request.cookies.get("role")?.value || "admin";
+
+  if (!token) {
+    if (
+      protectedRoutes.some((route) => pathname.startsWith(route)) ||
+      adminRoutes.some((route) => pathname.startsWith(route))
+    ) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
+  }
+
+  if (token && authRoutes.some((route) => pathname.startsWith(route))) {
+    return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+  }
+
+  if (adminRoutes.some((route) => pathname.startsWith(route))) {
+    if (role !== "admin") {
+        return NextResponse.redirect(new URL("/admin/dashboard", request.url));
+    }
+  }
+}
+
+export const config = {
+    matcher: [
+        "/admin/:path",
+        "/signin",
+    ]
+}
