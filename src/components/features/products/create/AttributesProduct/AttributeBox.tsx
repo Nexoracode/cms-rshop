95cf67e3481ad9602f683dad3503b1e627e292a5
mh.kadem <khadem5939@gmail.com>
2026-01-27 08:53:16 +0330

    Test New Font

--- src/components/features/products/create/AttributesProduct/AttributeBox.tsx
+++ src/components/features/products/create/AttributesProduct/AttributeBox.tsx
@@ -45,43 +45,50 @@ const AttributeBox: React.FC<AttributeBoxProps> = ({
         {/* isRequired={isDisabledEdit} */}
         <div className="p-2 rounded-xl mt-3 border border-slate-300 max-h-[200px] h-full">
           <ul className="flex flex-col overflow-y-auto overflow-hidden max-h-[180px]">
-            {attr?.length
-              ? attr.map((item: any, index) => (
-                  <li
-                    key={item.id}
-                    className={`border-slate-200 text-gray-700 flex items-center justify-between mx-2 p-2 py-1 group ${
-                      item.name ? "cursor-pointer" : "cursor-auto"
-                    } ${
-                      selectedAttrGroupId === item.id ? "bg-slate-100" : ""
-                    } ${attr?.length - 1 !== index ? "border-b" : ""}`}
-                    onClick={() => {
-                      if (item.name) {
-                        setSelectedAttrGroupId(+item.id);
-                        onChoose(+item.id);
-                      }
-                    }}
-                  >
-                    <span>{item.name || item.value}</span>
-                    <div className={`opacity-0 group-hover:opacity-100`}>
-                      <div className="flex items-center gap-2">
-                        <DeleteButton
-                          onDelete={() => {
-                            deleteAttr(item.id);
-                          }}
-                        />
-                        <div
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            onEdit(+item.id);
-                          }}
-                        >
-                          {children}
-                        </div>
+            {attr?.length ? (
+              attr.map((item: any, index) => (
+                <li
+                  key={item.id}
+                  className={`border-slate-200 text-gray-700 flex items-center justify-between mx-2 p-2 py-1 group ${
+                    item.name ? "cursor-pointer" : "cursor-auto"
+                  } ${selectedAttrGroupId === item.id ? "bg-slate-100" : ""} ${
+                    attr?.length - 1 !== index ? "border-b" : ""
+                  }`}
+                  onClick={() => {
+                    if (item.name) {
+                      setSelectedAttrGroupId(+item.id);
+                      onChoose(+item.id);
+                    }
+                  }}
+                >
+                  <span>{item.name || item.value}</span>
+                  <div className={`opacity-0 group-hover:opacity-100`}>
+                    <div className="flex items-center gap-2">
+                      <DeleteButton
+                        onDelete={() => {
+                          deleteAttr(item.id);
+                        }}
+                      />
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          onEdit(+item.id);
+                        }}
+                      >
+                        {children}
                       </div>
                     </div>
-                  </li>
-                ))
-              : ""}
+                  </div>
+                </li>
+              ))
+            ) : (
+              <div>
+                <p>
+                  برای نشان دادن لیست ابتدا مقدار مورد نظر را از لیست بالا
+                  انتخاب نمایید
+                </p>
+              </div>
+            )}
           </ul>
         </div>
       </div>
