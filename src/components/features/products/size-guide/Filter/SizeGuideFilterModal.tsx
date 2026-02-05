b901dc8c8d503e5d5d4ca7cd9317a4b1f28ff456
mh.kadem <khadem5939@gmail.com>
2026-02-05 10:39:42 +0330

    Update SizeGauideModal

--- src/components/features/products/size-guide/Filter/SizeGuideFilterModal.tsx
+++ src/components/features/products/size-guide/Filter/SizeGuideFilterModal.tsx
@@ -3,14 +3,39 @@
 import { useMemo } from "react";
 import FilterModal from "@/components/ui/modals/FilterModal/FilterModal";
 import { FilterField } from "@/components/ui/modals/FilterModal";
+import { useGetProducts } from "@/core/hooks/api/products/useProduct";
+import { useListQueryParams } from "@/core/hooks/common/useListQueryParams";
 
 const SizeGuideFilterModal: React.FC = () => {
-  const fields: FilterField[] = useMemo(
-    () => [
-      { key: "createdAt", label: "تاریخ ثبت سفارش", type: "dateRange" },
-    ],
-    []
+  const { search: SearchInProducts } = useListQueryParams({
+    searchKey: "search-product",
+  });
+
+  const { data: productsData } = useGetProducts({
+    page: 1,
+    search: SearchInProducts,
+  });
+
+  const products = useMemo(
+    () =>
+      productsData?.data?.items?.map((b: any) => ({
+        key: String(b.id),
+        title: b.name,
+      })) || [],
+    [productsData?.data?.items],
   );
+
+  const fields: FilterField[] = [
+    {
+      key: "product_id",
+      label: "محصولات",
+      type: "select",
+      options: products,
+      searchable: true,
+      syncSearchToUrl: true,
+      searchKey: "search-product",
+    },
+  ];
 
   return <FilterModal title="فیلتر راهنمای سایز" fields={fields} />;
 };
