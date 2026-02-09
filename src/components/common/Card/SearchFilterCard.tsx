fd7acbb76de0fb9d742db9472d7d287d3b918f63
mh.kadem <khadem5939@gmail.com>
2026-02-09 12:02:27 +0330

    Update

--- src/components/common/Card/SearchFilterCard.tsx
+++ src/components/common/Card/SearchFilterCard.tsx
@@ -28,18 +28,6 @@ const SearchFilterCard: React.FC<SearchFilterCardProps> = ({
   return (
     <Card className="shadow-md">
       <CardBody className="flex flex-col gap-4">
-        {showSearchBar || children ? (
-          <div className="flex flex-col sm:flex-row items-center gap-2 rounded-xl p-2">
-            {showSearchBar && (
-              <DebouncedSearchURL placeholder={searchPlaceholder} />
-            )}
-            <div className="w-full sm:w-fit flex items-center gap-2">
-              {children}
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
         {relatedPages.length > 0 && (
           <div className="flex flex-col sm:flex-row sm:items-center justify-between bg-slate-50 gap-4 text-start rounded-xl p-2">
             <p className="pr-2">{relatedTitle}</p>
@@ -55,6 +43,18 @@ const SearchFilterCard: React.FC<SearchFilterCardProps> = ({
               ))}
             </div>
           </div>
+        )}
+        {showSearchBar || children ? (
+          <div className="flex flex-col sm:flex-row items-center gap-2 rounded-xl p-2">
+            {showSearchBar && (
+              <DebouncedSearchURL placeholder={searchPlaceholder} />
+            )}
+            <div className="w-full sm:w-fit flex items-center gap-2">
+              {children}
+            </div>
+          </div>
+        ) : (
+          ""
         )}
       </CardBody>
     </Card>
