4b28f7628168262f90fb083cadfaa980e6253489
mh.kadem <khadem5939@gmail.com>
2026-02-14 14:37:06 +0330

    Update Gift Wrapping

--- src/app/admin/orders/gift-wrapping/create/page.tsx
+++ src/app/admin/orders/gift-wrapping/create/page.tsx
@@ -1,13 +1,19 @@
-"use client"
+"use client";
 
 import { useGetOneGiftWrapping } from "@/core/hooks/api/useGiftWrapping";
 import GiftWrappingForm from "@/components/features/orders/gift-wrapping/GiftWrappingForm";
 import { useFetchOnEdit } from "@/core/hooks/common/useFetchOnEdit";
+import Breadcrumbs from "@/components/common/Breadcrumbs";
 
 const GiftWrappingCreate = () => {
   const { data, isLoading, editId } = useFetchOnEdit(useGetOneGiftWrapping);
 
-  return <GiftWrappingForm data={data} isLoading={isLoading} id={editId}/>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Breadcrumbs />
+      <GiftWrappingForm data={data} isLoading={isLoading} id={editId} />
+    </div>
+  );
 };
 
 export default GiftWrappingCreate;
