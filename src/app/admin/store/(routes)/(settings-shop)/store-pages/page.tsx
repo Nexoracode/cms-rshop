246b3e1fa763d819767db2c906220e4416681f87
mh.kadem <khadem5939@gmail.com>
2026-02-16 12:33:07 +0330

    Update Store Page

--- src/app/admin/store/(routes)/(settings-shop)/store-pages/page.tsx
+++ src/app/admin/store/(routes)/(settings-shop)/store-pages/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import { BsInfoCircle } from "react-icons/bs";
-import {
-  TbCreditCardRefund,
-  TbWorldSearch,
-} from "react-icons/tb";
+import { TbCreditCardRefund, TbWorldSearch } from "react-icons/tb";
 import BaseCard from "@/components/ui/BaseCard";
 import { TfiShoppingCartFull } from "react-icons/tfi";
 import { LuMessageCircleQuestion } from "react-icons/lu";
 import Link from "next/link";
 
 const AboutStore = () => {
-
   const pages = [
     {
       type: "about_us",
@@ -41,50 +37,68 @@ const AboutStore = () => {
       CardHeaderProps={{
         title: "صفحات فروشگاه",
         icon: <TbWorldSearch />,
-        tooltipTitle: "مدیریت صفحات محتوایی فروشگاه",
-        tooltipDescription:
-          "در این بخش می‌توانید صفحات ثابت و اطلاع‌رسانی فروشگاه مانند درباره ما، راهنمای خرید، قوانین بازگشت کالا و سوالات متداول را ایجاد، ویرایش و فعال یا غیرفعال کنید. این صفحات مستقیماً در سایت به کاربران نمایش داده می‌شوند و نقش مهمی در اعتمادسازی و راهنمایی مشتریان دارند.",
+        showIconInActionSlot: true,
       }}
-      bodyClassName="grid grid-cols-1 sm:grid-cols-2 gap-4"
     >
-      {/* Store Pages */}
-      {pages.map((page) => {
-        const Icon = page.icon;
+      <div className="w-full flex items-center justify-between px-4">
+        <div>
+          <h2 className="text-xl font-bold text-gray-800 mb-4">
+            راهنمای صفحات فروشگاه
+          </h2>
+          <p className="text-gray-600  leading-7">
+            در این بخش می‌توانید صفحات ثابت و اطلاع‌رسانی فروشگاه مانند درباره
+            ما، راهنمای خرید، قوانین بازگشت کالا و سوالات متداول را ایجاد،
+            ویرایش و فعال یا غیرفعال کنید. این صفحات مستقیماً در سایت به کاربران
+            نمایش داده می‌شوند و نقش مهمی در اعتمادسازی و راهنمایی مشتریان
+            دارند.
+          </p>
+        </div>
+        <img src="/images/why-us.jpg" alt="چرا ما" className="w-72" />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {/* Store Pages */}
+        {pages.map((page) => {
+          const Icon = page.icon;
 
-        return (
-          <Link
-            key={page.type}
-            href={`/admin/store/store-pages/${page.type}`}
-            className="cursor-pointer rounded-2xl border border-gray-200 p-5 flex items-center gap-4 hover:shadow-md transition-all"
-          >
-            <div
-              className={`w-12 h-12 flex items-center justify-center rounded-xl text-2xl ${page.color}`}
+          return (
+            <Link
+              key={page.type}
+              href={`/admin/store/store-pages/${page.type}`}
+              className="cursor-pointer rounded-2xl border border-gray-200 p-5 flex items-center gap-4 hover:shadow-md transition-all"
             >
-              <Icon />
-            </div>
+              <div
+                className={`w-12 h-12 flex items-center justify-center rounded-xl text-2xl ${page.color}`}
+              >
+                <Icon />
+              </div>
 
-            <div className="flex flex-col">
-              <span className="font-semibold text-gray-800">{page.title}</span>
-              <span className="text-sm text-gray-500">{page.description}</span>
-            </div>
-          </Link>
-        );
-      })}
+              <div className="flex flex-col">
+                <span className="font-semibold text-gray-800">
+                  {page.title}
+                </span>
+                <span className="text-sm text-gray-500">
+                  {page.description}
+                </span>
+              </div>
+            </Link>
+          );
+        })}
 
-      {/* FAQ */}
-      <Link
-        href={"/admin/store/store-pages/faqs"}
-        className="cursor-pointer rounded-2xl border border-gray-200 p-5 flex items-center gap-4 hover:shadow-md transition-all"
-      >
-        <div className="w-12 h-12 flex items-center justify-center rounded-xl text-2xl text-green-700 bg-green-700/10">
-          <LuMessageCircleQuestion />
-        </div>
+        {/* FAQ */}
+        <Link
+          href={"/admin/store/store-pages/faqs"}
+          className="cursor-pointer rounded-2xl border border-gray-200 p-5 flex items-center gap-4 hover:shadow-md transition-all"
+        >
+          <div className="w-12 h-12 flex items-center justify-center rounded-xl text-2xl text-green-700 bg-green-700/10">
+            <LuMessageCircleQuestion />
+          </div>
 
-        <div className="flex flex-col">
-          <span className="font-semibold text-gray-800">سوالات متداول</span>
-          <span className="text-sm text-gray-500">مدیریت FAQ فروشگاه</span>
-        </div>
-      </Link>
+          <div className="flex flex-col">
+            <span className="font-semibold text-gray-800">سوالات متداول</span>
+            <span className="text-sm text-gray-500">مدیریت FAQ فروشگاه</span>
+          </div>
+        </Link>
+      </div>
     </BaseCard>
   );
 };
