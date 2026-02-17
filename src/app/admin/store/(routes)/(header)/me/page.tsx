cf811dd82a3413619be727615f2147cd5c1952c8
mh.kadem <khadem5939@gmail.com>
2026-02-17 11:01:45 +0330

    Update

--- src/app/admin/store/(routes)/(header)/me/page.tsx
+++ src/app/admin/store/(routes)/(header)/me/page.tsx
@@ -2,45 +2,56 @@
 
 // Components
 import UnifiedCard from "@/components/common/Card/UnifiedCard";
-import CustomersFilter from "@/components/features/store/customers/Filter/CustomersFilter";
 import CustomerCard from "@/components/features/store/customers/CustomerCard";
 import AddNewCustomerModal from "@/components/features/store/customers/modals/AddNewCustomerModal";
 // Icons
 import { UserSortBy, useGetAllUsers } from "@/core/hooks/api/users/useUsers";
-import { FiUsers } from "react-icons/fi";
 import { useListQueryParams } from "@/core/hooks/common/useListQueryParams";
+import { HiOutlineUserGroup } from "react-icons/hi2";
+import { GrUserAdmin } from "react-icons/gr";
+import { useGetAdminMe } from "@/core/hooks/api/useUsersAdmin";
 
 const Me = () => {
-  const { page, sortBy, search, filter, isFilteredView } =
-    useListQueryParams<UserSortBy[number]>();
+  const { page } = useListQueryParams<UserSortBy[number]>();
 
-  const { data: users, isLoading } = useGetAllUsers({
+  const { data: admin, isLoading: isLoadingAdminData } = useGetAdminMe();
+
+  const { data: users, isLoading: isLoadingAdminsData } = useGetAllUsers({
     page,
-    filter,
-    search,
-    sortBy,
   });
 
   const isExistItems = !!users?.data?.items?.length;
 
+  console.log("admin =>", admin);
+  
+
   return (
-    <UnifiedCard
-      searchFilter={<CustomersFilter />}
-      headerProps={{
-        title: "لیست کاربران",
-        icon: <FiUsers className="text-2xl" />,
-        children: <AddNewCustomerModal />,
-      }}
-      isLoading={isLoading}
-      isExistItems={isExistItems}
-      searchInp={isFilteredView}
-      meta={users?.data?.meta}
-      childrenClassName="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center md:justify-items-stretch"
-    >
-      {users?.data?.items?.map((user: any) => (
-        <CustomerCard key={user.id} infos={user} />
-      ))}
-    </UnifiedCard>
+    <div className="flex flex-col gap-4">
+      <UnifiedCard
+        headerProps={{
+          title: "اطلاعات من",
+          icon: <GrUserAdmin className="text-2xl" />,
+          showIconInActionSlot: true,
+        }}
+        isLoading={isLoadingAdminData}
+        isExistItems={isExistItems}
+      ></UnifiedCard>
+      <UnifiedCard
+        headerProps={{
+          title: "لیست کارمندان فروشگاه",
+          icon: <HiOutlineUserGroup className="text-2xl" />,
+          children: <AddNewCustomerModal />,
+        }}
+        isLoading={isLoadingAdminsData}
+        isExistItems={isExistItems}
+        meta={users?.data?.meta}
+        childrenClassName="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center md:justify-items-stretch"
+      >
+        {users?.data?.items?.map((user: any) => (
+          <CustomerCard key={user.id} infos={user} />
+        ))}
+      </UnifiedCard>
+    </div>
   );
 };
 
